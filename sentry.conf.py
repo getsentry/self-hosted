f21738f7e5398836e37c0e6f002e041d6f73128f
Adrian Gomez <adri4n.steam@gmail.com>
2017-10-16 16:10:14 -0300

    Add Procfile to run all services on aptible

--- sentry.conf.py
+++ sentry.conf.py
@@ -244,7 +244,7 @@ if env('SENTRY_USE_SSL', False):
 SENTRY_WEB_HOST = '0.0.0.0'
 SENTRY_WEB_PORT = 9000
 SENTRY_WEB_OPTIONS = {
-    # 'workers': 3,  # the number of web workers
+    'workers': 3,  # the number of web workers
 }
 
 ###############
