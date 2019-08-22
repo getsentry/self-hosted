c0881e3393d826c6753e9aecd0660b21a0a8df2f
Burak Yigit Kaya <ben@byk.im>
2019-08-22 20:32:56 +0200

    Move to a more generic and env-friendly solution

---
+++ cron/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+# Prior art:
+# - https://git.io/fjNOg
+# - https://blog.knoldus.com/running-a-cron-job-in-docker-container/
+
+declare -p | grep -Ev 'BASHOPTS|BASH_VERSINFO|EUID|PPID|SHELLOPTS|UID' > /container.env
+
+{ for cron_job in "$@"; do echo -e "SHELL=/bin/bash
+BASH_ENV=/container.env
+${cron_job} > /proc/1/fd/1 2>/proc/1/fd/2"; done } \
+    | sed --regexp-extended 's/\\(.)/\1/g' \
+    | crontab -
+crontab -l
+exec cron -f -l -L 15
