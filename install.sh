65475c8bebed905d0b7aa8018997e294e30cc834
Burak Yigit Kaya <ben@byk.im>
2020-01-03 13:04:57 +0300

    add reference to StackOverflow as the source

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 set -e
 
+# Thanks to https://unix.stackexchange.com/a/145654/108960
 log_file="sentry_install_log-`date +'%Y-%m-%d_%H-%M-%S'`.txt"
 exec &> >(tee -a "$log_file")
 
