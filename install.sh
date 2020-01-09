bc234481414062294f1fbdb9c0573555bf1b34b0
Simon Golms <simongolms@aol.com>
2020-01-09 20:55:20 +0100

    fix(install): support for Windows and Git Bash (#335)
    Fixes #329.

--- install.sh
+++ install.sh
@@ -151,7 +151,8 @@ SENTRY_DATA_NEEDS_MIGRATION=$(docker run --rm -v sentry-data:/data alpine ash -c
 if [ "$SENTRY_DATA_NEEDS_MIGRATION" ]; then
   echo "Migrating file storage..."
   # Use the web (Sentry) image so the file owners are kept as sentry:sentry
-  $dcr --entrypoint /bin/bash web -c \
+  # The `\"` escape pattern is to make this compatible w/ Git Bash on Windows. See #329.
+  $dcr --entrypoint \"/bin/bash\" web -c \
     "mkdir -p /tmp/files; mv /data/* /tmp/files/; mv /tmp/files /data/files"
 fi
 
