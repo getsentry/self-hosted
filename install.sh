de7b102f91dbffd9ebaf8e73df3b6760e58f7978
Burak Yigit Kaya <byk@sentry.io>
2020-04-24 15:13:38 +0300

    ref(install): Reduce noise on docker-compose pull (#442)

--- install.sh
+++ install.sh
@@ -102,11 +102,18 @@ if grep -xq "system.secret-key: '!!changeme!!'" $SENTRY_CONFIG_YML ; then
 fi
 
 echo ""
+echo "Fetching and updating Docker images..."
+echo ""
+# We tag locally built images with an '-onpremise-local' suffix. docker-compose pull tries to pull these too and
+# shows a 404 error on the console which is confusing and unnecessary. To overcome this, we add the stderr>stdout
+# redirection below and pass it through grep, ignoring all lines having this '-onpremise-local' suffix.
+$dc pull -q --ignore-pull-failures 2>&1 | grep -v -- -onpremise-local || true
+docker pull ${SENTRY_IMAGE:-getsentry/sentry:latest}
+
+echo ""
 echo "Building and tagging Docker images..."
 echo ""
 # Build the sentry onpremise image first as it is needed for the cron image
-$dc pull --ignore-pull-failures
-docker pull ${SENTRY_IMAGE:-getsentry/sentry:latest}
 $dc build --force-rm web
 $dc build --force-rm --parallel
 echo ""
