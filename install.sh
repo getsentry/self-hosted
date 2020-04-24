0cb67a77be1ec3726a58641692393a1b14afa484
Burak Yigit Kaya <ben@byk.im>
2020-04-24 15:05:36 +0300

    typo: build -> built

--- install.sh
+++ install.sh
@@ -104,7 +104,7 @@ fi
 echo ""
 echo "Fetching and updating Docker images..."
 echo ""
-# We tag locally build images with an '-onpremise-local' suffix. docker-compose pull tries to pull these too and
+# We tag locally built images with an '-onpremise-local' suffix. docker-compose pull tries to pull these too and
 # shows a 404 error on the console which is confusing and unnecessary. To overcome this, we add the stderr>stdout
 # redirection below and pass it through grep, ignoring all lines having this '-onpremise-local' suffix.
 $dc pull -q --ignore-pull-failures 2>&1 | grep -v -- -onpremise-local || true
