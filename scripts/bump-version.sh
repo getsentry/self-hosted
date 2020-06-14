276037fa49e7ddef1ee58995d52c1c8ff8eede74
Burak Yigit Kaya <ben@byk.im>
2020-06-14 22:12:44 +0300

    build(gha): Add automated CalVer releases

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -eux
+set -eu
 
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 cd $SCRIPT_DIR/..
@@ -7,5 +7,11 @@ cd $SCRIPT_DIR/..
 OLD_VERSION="$1"
 NEW_VERSION="$2"
 
-VERSION=$2
-echo "New version: $VERSION"
+SYMBOLICATOR_VERSION=$(curl -sSL 'https://api.github.com/repos/getsentry/symbolicator/git/refs/heads/master' | grep -Po '(?<=\"sha\": \")([a-f0-9]{5,40})(?=\",?)')
+
+sed -i -e "s/^SYMBOLICATOR_VERSION=.*\$/SYMBOLICATOR_VERSION=$SYMBOLICATOR_VERSION/" .env
+sed -i -e "s/^SENTRY_VERSION=.*\$/SENTRY_VERSION=$NEW_VERSION/" .env
+sed -i -e "s/^\# Sentry .* On-Premise/# Sentry $NEW_VERSION On-Premise/" README.md
+
+echo "New version: $NEW_VERSION"
+echo "New Symbolicator version: $SYMBOLICATOR_VERSION"
