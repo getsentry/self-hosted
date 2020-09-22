0514836ba44526f17b5f7fef819399599ef9e14b
Burak Yigit Kaya <ben@byk.im>
2020-09-22 23:49:54 +0300

    ingest events too

--- install.sh
+++ install.sh
@@ -248,7 +248,7 @@ echo ""
 # NOTE: This step relies on `kafka` being available from the previous `snuba-api bootstrap` step
 # XXX(BYK): We cannot use auto.create.topics as Confluence and Apache hates it now (and makes it very hard to enable)
 EXISTING_KAFKA_TOPICS=$($dcr kafka kafka-topics --list --bootstrap-server kafka:9092 2>/dev/null)
-NEEDED_KAFKA_TOPICS="ingest-attachments ingest-transactions"
+NEEDED_KAFKA_TOPICS="ingest-attachments ingest-transactions ingest-events"
 for topic in $NEEDED_KAFKA_TOPICS; do
   if ! echo "$EXISTING_KAFKA_TOPICS" | grep -wq ingest-attachments; then
     echo "Creating additional Kafka topics..."
