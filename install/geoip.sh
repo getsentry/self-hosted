f208f0941ed147eb865a3d736280f0031f872f48
Chad Whitacre <chadwhitacre@sentry.io>
2020-12-11 11:16:55 -0500

    Better encapsulate geoip installation

--- install/geoip.sh
+++ install/geoip.sh
@@ -5,30 +5,35 @@ if [ ! -f 'install.sh' ]; then echo 'Where are you?'; exit 1; fi
 dc="docker-compose --no-ansi"
 dcr="$dc run --rm"
 
-GEOLITE2_CITY_MMDB='geoip/GeoLite2-City.mmdb'
-GEOIP_CONF='geoip/GeoIP.conf'
-result='Done'
 
-echo "Setting up IP address geolocation ..."
-if [[ ! -f "$GEOLITE2_CITY_MMDB" ]]; then
-  echo -n "Installing (empty) IP address geolocation database ... "
-  cp "$GEOLITE2_CITY_MMDB.empty" "$GEOLITE2_CITY_MMDB"
-  echo "done."
-else
-  echo "IP address geolocation database already exists."
-fi
+install_geoip() {
+  local mmdb='geoip/GeoLite2-City.mmdb'
+  local conf='geoip/GeoIP.conf'
+  local result='Done'
 
-if [[ ! -f "$GEOIP_CONF" ]]; then
-  echo "IP address geolocation is not configured for updates."
-  echo "See https://develop.sentry.dev/self-hosted/geolocation/ for instructions."
-  result='Error'
-else
-  echo "IP address geolocation is configured for updates."
-  echo "Updating IP address geolocation database ... "
-  $dcr geoipupdate
-  if [ $? -gt 0 ]; then
-    result='Error'
+  echo "Setting up IP address geolocation ..."
+  if [[ ! -f "$mmdb" ]]; then
+    echo -n "Installing (empty) IP address geolocation database ... "
+    cp "$mmdb.empty" "$mmdb"
+    echo "done."
+  else
+    echo "IP address geolocation database already exists."
   fi
-  echo "$result updating IP address geolocation database."
-fi
-echo "$result setting up IP address geolocation."
+
+  if [[ ! -f "$conf" ]]; then
+    echo "IP address geolocation is not configured for updates."
+    echo "See https://develop.sentry.dev/self-hosted/geolocation/ for instructions."
+    result='Error'
+  else
+    echo "IP address geolocation is configured for updates."
+    echo "Updating IP address geolocation database ... "
+    $dcr geoipupdate
+    if [ $? -gt 0 ]; then
+      result='Error'
+    fi
+    echo "$result updating IP address geolocation database."
+  fi
+  echo "$result setting up IP address geolocation."
+}
+
+install_geoip
