1d791fc1c69bcfed10c26ae2607eb99fa23f1f8e
Burak Yigit Kaya <ben@byk.im>
2021-01-14 23:49:49 +0300

    nopeless debugging

--- install/py2-warning.sh
+++ install/py2-warning.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
 if [[ ! -f 'install.sh' ]]; then echo 'Where are you?'; exit 1; fi
-
-if docker-compose --no-ansi run --rm --no-deps --entrypoint python web --version | grep -q 'Python 2'; then
+echo "A"
+source ./install/docker-aliases.sh
+echo "B"
+if echo "C" && $dcr --no-deps --entrypoint python web --version | grep -q 'Python 2'; then
+echo "D";
   WARNING_TEXT="
  _  _   ____      ____  _       _______     ____  _____  _____  ____  _____   ______   _  _
 | || | |_  _|    |_  _|/ \     |_   __ \   |_   \|_   _||_   _||_   \|_   _|.' ___  | | || |
@@ -12,7 +15,9 @@ if docker-compose --no-ansi run --rm --no-deps --entrypoint python web --version
 (_)(_)      \/  \/|____| |____||____| |___||_____|\____||_____||_____|\____|\`._____.' (_)(_)
 
 "
+echo "E"
   echo "$WARNING_TEXT"
+echo "F"
   echo '-----------------------------------------------------------'
   echo 'You are using Sentry with Python 2, which is deprecated.'
   echo 'Sentry 21.1 will be the last version with Python 2 support.'
