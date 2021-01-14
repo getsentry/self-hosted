a1f6c69c833c3126120cf06f054219adb789e7d1
Burak Yigit Kaya <ben@byk.im>
2021-01-15 00:56:41 +0300

    just use echo

--- install/py2-warning.sh
+++ install/py2-warning.sh
@@ -7,7 +7,7 @@ source ./install/docker-aliases.sh
 # Note the stderr>stdout redirection because Python thinks `--version` should
 # be on stderr: https://stackoverflow.com/a/31715011/90297
 if $dcr --no-deps --entrypoint python web --version 2>&1 | grep -q 'Python 2'; then
-  WARNING_TEXT="
+  echo "
  _  _   ____      ____  _       _______     ____  _____  _____  ____  _____   ______   _  _
 | || | |_  _|    |_  _|/ \     |_   __ \   |_   \|_   _||_   _||_   \|_   _|.' ___  | | || |
 | || |   \ \  /\  / / / _ \      | |__) |    |   \ | |    | |    |   \ | | / .'   \_| | || |
@@ -16,7 +16,6 @@ if $dcr --no-deps --entrypoint python web --version 2>&1 | grep -q 'Python 2'; t
 (_)(_)      \/  \/|____| |____||____| |___||_____|\____||_____||_____|\____|\`._____.' (_)(_)
 
 "
-  echo "$WARNING_TEXT"
   echo '-----------------------------------------------------------'
   echo 'You are using Sentry with Python 2, which is deprecated.'
   echo 'Sentry 21.1 will be the last version with Python 2 support.'
