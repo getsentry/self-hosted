d8de124610f3935bec0235ea97fd1d34c8e6b3c3
Burak Yigit Kaya <ben@byk.im>
2021-01-14 19:11:34 +0300

    try again; set +x

--- install/py2-warning.sh
+++ install/py2-warning.sh
@@ -5,9 +5,8 @@ if [ ! -f 'install.sh' ]; then echo 'Where are you?'; exit 1; fi
 source ./install/docker-aliases.sh
 
 py2_warning() {
-    $dcr --no-deps --entrypoint python web --version | grep -q 'Python 2'
-    if [[ $? -eq 0 ]]; then
-    cat <<"EOW"
+    if [[ -n $($dcr --no-deps --entrypoint python web --version | grep 'Python 2') ]]; then
+      cat <<"EOW"
  _  _   ____      ____  _       _______     ____  _____  _____  ____  _____   ______   _  _
 | || | |_  _|    |_  _|/ \     |_   __ \   |_   \|_   _||_   _||_   \|_   _|.' ___  | | || |
 | || |   \ \  /\  / / / _ \      | |__) |    |   \ | |    | |    |   \ | | / .'   \_| | || |
@@ -16,8 +15,8 @@ py2_warning() {
 (_)(_)      \/  \/|____| |____||____| |___||_____|\____||_____||_____|\____|`._____.' (_)(_)
 
 EOW
-        echo 'You are using Sentry with Python 2, which is deprecated.'
-        echo 'Sentry 21.1 will be the last version with Python 2 support.'
+      echo 'You are using Sentry with Python 2, which is deprecated.'
+      echo 'Sentry 21.1 will be the last version with Python 2 support.'
     fi
 }
 
