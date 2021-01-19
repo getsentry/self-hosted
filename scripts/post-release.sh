af7f50c883bf3d3144bc68db22af6f3657302321
Burak Yigit Kaya <byk@sentry.io>
2021-01-19 20:56:13 +0300

    ci(release): Make sure to pull before pushing in post-release (#820)
    Fixes the issue we had over at getsentry/publish#60. This aligns the script with [the one at getsentry/sentry](https://github.com/getsentry/sentry/blob/master/scripts/post-release.sh#L6)

--- scripts/post-release.sh
+++ scripts/post-release.sh
@@ -7,4 +7,4 @@ cd $SCRIPT_DIR/..
 # Bring master back to nightlies after merge from release branch
 
 SYMBOLICATOR_VERSION=nightly ./scripts/bump-version.sh '' 'nightly'
-git diff --quiet || git commit -anm 'build: Set master version to nightly' && git push
+git diff --quiet || git commit -anm 'build: Set master version to nightly' && git pull --rebase && git push
