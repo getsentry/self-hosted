2b23f4e2a533c9f4a1a3798488ec80a36f0de9b5
Burak Yigit Kaya <ben@byk.im>
2021-01-23 23:39:39 +0300

    don't fail when checksum is missing

--- sentry/entrypoint.sh
+++ sentry/entrypoint.sh
@@ -4,7 +4,7 @@ set -e
 req_file="/etc/sentry/requirements.txt"
 checksum_file="/data/custom-packages/.checksum"
 
-if [[ -s "$req_file" ]] && ! cat "$req_file" | grep '^[^#[:space:]]' | shasum -s -c "$checksum_file" 2>/dev/null; then
+if [[ -s "$req_file" ]] && [[ ! -f "$checksum_file" ]] || ! cat "$req_file" | grep '^[^#[:space:]]' | shasum -s -c "$checksum_file" 2>/dev/null; then
     echo "Installing additional dependencies..."
     pip install --user -r /etc/sentry/requirements.txt
     cat "$req_file" | grep '^[^#[:space:]]' | shasum > "$checksum_file"
