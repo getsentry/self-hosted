498413b2f9f4933f7e144b42b6ac26d8acb473ca
Burak Yigit Kaya <ben@byk.im>
2021-01-23 23:40:04 +0300

    dry

--- sentry/entrypoint.sh
+++ sentry/entrypoint.sh
@@ -6,7 +6,7 @@ checksum_file="/data/custom-packages/.checksum"
 
 if [[ -s "$req_file" ]] && [[ ! -f "$checksum_file" ]] || ! cat "$req_file" | grep '^[^#[:space:]]' | shasum -s -c "$checksum_file" 2>/dev/null; then
     echo "Installing additional dependencies..."
-    pip install --user -r /etc/sentry/requirements.txt
+    pip install --user -r "$req_file"
     cat "$req_file" | grep '^[^#[:space:]]' | shasum > "$checksum_file"
     echo ""
 fi
