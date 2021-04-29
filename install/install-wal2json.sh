47e95d607b70a4ab5c0fffe9f288a8ad9b97731d
Filippo Pacifici <filippo.pacifici@gmail.com>
2021-04-28 22:24:41 -0700

    Pin version when releasing

--- install/install-wal2json.sh
+++ install/install-wal2json.sh
@@ -1,29 +1,31 @@
 echo "${_group}Downloading and installing wal2json ..."
 
-LATEST_VERSION_FILE="../postgres/wal2json/wal2json.so"
+VERSION_FILE="../postgres/wal2json/wal2json.so"
 ARCH=$(uname -m)
 FILE_NAME="wal2json-Linux-$ARCH.so"
-LATEST_VERSION=$(
-    wget "https://api.github.com/repos/getsentry/wal2json/releases/latest" -O - |
-    grep '"tag_name":' |
-    sed -E 's/.*"([^"]+)".*/\1/'
-)
 
-mkdir -p ../postgres/wal2json
-if [[ $LATEST_VERSION ]]; then
-    if [ ! -f "../postgres/wal2json/$LATEST_VERSION/$FILE_NAME" ]; then
-        mkdir -p "../postgres/wal2json/$LATEST_VERSION"
-        if wget \
-            "https://github.com/getsentry/wal2json/releases/download/$LATEST_VERSION/$FILE_NAME" \
-            -P "../postgres/wal2json/$LATEST_VERSION/"; then
-            ln -s "`pwd`/../postgres/wal2json/$LATEST_VERSION/$FILE_NAME" "$LATEST_VERSION_FILE"
-        fi
+if [[ $WAL2JSON_VERSION == "latest" ]]; then
+    VERSION=$(
+        wget "https://api.github.com/repos/getsentry/wal2json/releases/latest" -O - |
+        grep '"tag_name":' |
+        sed -E 's/.*"([^"]+)".*/\1/'
+    )
+
+    if [[ ! $VERSION ]]; then
+        echo "Cannot find wal2json latest version"
+        exit 1
     fi
 else
-    echo "wal2json is not installed and cannot download latest version"
-    exit 1
+    VERSION=$WAL2JSON_VERSION
 fi
 
-set -e
+mkdir -p ../postgres/wal2json
+if [ ! -f "../postgres/wal2json/$VERSION/$FILE_NAME" ]; then
+    mkdir -p "../postgres/wal2json/$VERSION"
+    wget \
+        "https://github.com/getsentry/wal2json/releases/download/$VERSION/$FILE_NAME" \
+        -P "../postgres/wal2json/$VERSION/"; then
+    ln -s "`pwd`/../postgres/wal2json/$VERSION/$FILE_NAME" "$VERSION_FILE"
+fi  
 
 echo "${_endgroup}"
