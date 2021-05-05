9d2f8a70638ca0c69f03153c01e266a64ea5761c
Filippo Pacifici <filippo.pacifici@gmail.com>
2021-05-04 18:11:02 -0700

    Do not rely on having wget

--- install/install-wal2json.sh
+++ install/install-wal2json.sh
@@ -4,9 +4,12 @@ FILE_TO_USE="../postgres/wal2json/wal2json.so"
 ARCH=$(uname -m)
 FILE_NAME="wal2json-Linux-$ARCH.so"
 
+DOCKER="docker run --rm"
+CURL="curlimages/curl"
+
 if [[ $WAL2JSON_VERSION == "latest" ]]; then
     VERSION=$(
-        wget "https://api.github.com/repos/getsentry/wal2json/releases/latest" -O - |
+        $DOCKER $CURL https://api.github.com/repos/getsentry/wal2json/releases/latest |
         grep '"tag_name":' |
         sed -E 's/.*"([^"]+)".*/\1/'
     )
@@ -22,10 +25,10 @@ fi
 mkdir -p ../postgres/wal2json
 if [ ! -f "../postgres/wal2json/$VERSION/$FILE_NAME" ]; then
     mkdir -p "../postgres/wal2json/$VERSION"
-    wget \
+    $DOCKER -v "`pwd`/../postgres/wal2json/$VERSION:/tmp" $CURL \
         "https://github.com/getsentry/wal2json/releases/download/$VERSION/$FILE_NAME" \
-        -P "../postgres/wal2json/$VERSION/"
-    cp "`pwd`/../postgres/wal2json/$VERSION/$FILE_NAME" "$FILE_TO_USE"
+        -o /tmp/$FILE_NAME
+    cp "../postgres/wal2json/$VERSION/$FILE_NAME" "$FILE_TO_USE"
 fi  
 
 echo "${_endgroup}"
