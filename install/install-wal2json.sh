b2a48a9a47d3e659e07cedbb8c434d90d2f62494
Filippo Pacifici <filippo.pacifici@gmail.com>
2021-05-04 19:04:32 -0700

    Try by redirecting output

--- install/install-wal2json.sh
+++ install/install-wal2json.sh
@@ -4,12 +4,11 @@ FILE_TO_USE="../postgres/wal2json/wal2json.so"
 ARCH=$(uname -m)
 FILE_NAME="wal2json-Linux-$ARCH.so"
 
-DOCKER="docker run --rm"
-CURL="curlimages/curl"
+DOCKER_CURL="docker run --rm curlimages/curl"
 
 if [[ $WAL2JSON_VERSION == "latest" ]]; then
     VERSION=$(
-        $DOCKER $CURL https://api.github.com/repos/getsentry/wal2json/releases/latest |
+        $DOCKER_CURL https://api.github.com/repos/getsentry/wal2json/releases/latest |
         grep '"tag_name":' |
         sed -E 's/.*"([^"]+)".*/\1/'
     )
@@ -25,12 +24,10 @@ fi
 mkdir -p ../postgres/wal2json
 if [ ! -f "../postgres/wal2json/$VERSION/$FILE_NAME" ]; then
     mkdir -p "../postgres/wal2json/$VERSION"
-    ls -al "../postgres/wal2json/"
-    id
-    $DOCKER -v "`pwd`/../postgres/wal2json/$VERSION/$FILE_NAME:/tmp" \
-        $CURL -u $(id -u ${USER}):$(id -g ${USER})\
+    $DOCKER_CURL -L \
         "https://github.com/getsentry/wal2json/releases/download/$VERSION/$FILE_NAME" \
-        -o /tmp/$FILE_NAME
+        > "../postgres/wal2json/$VERSION/$FILE_NAME"
+        
     cp "`pwd`/../postgres/wal2json/$VERSION/$FILE_NAME" "$FILE_TO_USE"
 fi  
 
