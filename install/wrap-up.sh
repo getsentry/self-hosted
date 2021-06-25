f5411170ac0fb7f5ce8e7f5a1b21b16cbb176937
fmartinou <manfred.martin@gmail.com>
2021-06-25 13:55:34 +0200

    Update middlewares to latest stable versions (#1002)
    This PR is a try to update most middlewares used by Sentry to latest stable versions.
    [As mentioned in the forum](https://forum.sentry.io/t/middleware-version-compatibility/14353/2) I didn't update Postgresql & Clickhouse due to known issues.
    
    I also :
    - changed versions to immutable tags (MAJOR.MINOR.PATCH semver versions when possible).
    - changed nginx to the Alpine variant

--- install/wrap-up.sh
+++ install/wrap-up.sh
@@ -3,7 +3,7 @@ if [[ "$MINIMIZE_DOWNTIME" ]]; then
 
   # Start the whole setup, except nginx and relay.
   $dc up -d --remove-orphans $($dc config --services | grep -v -E '^(nginx|relay)$')
-  $dc exec -T nginx service nginx reload
+  $dc exec -T nginx nginx -s reload
 
   docker run --rm --network="${COMPOSE_PROJECT_NAME}_default" alpine ash \
     -c 'while [[ "$(wget -T 1 -q -O- http://web:9000/_health/)" != "ok" ]]; do sleep 0.5; done'
