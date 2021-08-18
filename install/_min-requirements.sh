497759c1e83280f43cc481735f37eabd37873a13
Burak Yigit Kaya <byk@sentry.io>
2021-08-18 18:35:21 +0300

    ci: Test with the required minimum docker-compose (#1066)
    Refs #1061, refs #1062, refs #1063.

---
+++ install/_min-requirements.sh
@@ -0,0 +1,7 @@
+# Don't forget to update the README and othes docs when you change these!
+MIN_DOCKER_VERSION='19.03.6'
+MIN_COMPOSE_VERSION='1.28.0'
+MIN_RAM_HARD=3800 # MB
+MIN_RAM_SOFT=7800 # MB
+MIN_CPU_HARD=2
+MIN_CPU_SOFT=4
