920be6771d2ea0af88995613f8dabbb297899650
Burak Yigit Kaya <byk@sentry.io>
2021-10-15 23:14:06 +0300

    build: Omit nightly bump commit from changelog (#1120)

--- scripts/post-release.sh
+++ scripts/post-release.sh
@@ -7,4 +7,4 @@ cd $SCRIPT_DIR/..
 # Bring master back to nightlies after merge from release branch
 git checkout master && git pull
 SYMBOLICATOR_VERSION=nightly ./scripts/bump-version.sh '' 'nightly'
-git diff --quiet || git commit -anm 'build: Set master version to nightly' && git pull --rebase && git push
+git diff --quiet || git commit -anm $'build: Set master version to nightly\n\n#skip-changelog' && git pull --rebase && git push
