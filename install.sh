4f1a263392e48f975c6225340c74930313452523
Chad Whitacre <chadwhitacre@sentry.io>
2021-11-05 11:39:46 -0400

    Minimize test case further

--- install.sh
+++ install.sh
@@ -1,12 +1,4 @@
 #!/usr/bin/env bash
 set -e
-if [[ -n "$MSYSTEM" ]]; then
-  echo "Seems like you are using an MSYS2-based system (such as Git Bash) which is not supported. Please use WSL instead.";
-  exit 1
-fi
-
-source "$(dirname $0)/install/_lib.sh"  # does a `cd .../install/`, among other things
-
-source parse-cli.sh
-source error-handling.sh
-source set-up-and-migrate-database.sh
+exec &> >(tee -a "foo.log")
+docker compose --ansi never run --rm web upgrade
