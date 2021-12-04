b4db562186f33aff38f6fe907d6cd48dc9c506f5
Amin Vakil <info@aminvakil.com>
2021-12-04 16:20:48 +0330

    Only check for latest commit on master branch

--- install.sh
+++ install.sh
@@ -5,9 +5,11 @@ if [[ -n "$MSYSTEM" ]]; then
   exit 1
 fi
 
-if [[ $(git rev-parse HEAD) != $(git ls-remote $(git rev-parse --abbrev-ref @{u} | sed 's/\// /g') | cut -f1) ]]; then
-  echo "Seems like you are not using the latest commit from self-hosted repository. Please pull the latest changes and try again.";
-  exit 1
+if [[ $(git branch | sed -n '/\* /s///p') == "master" ]]; then
+  if [[ $(git rev-parse HEAD) != $(git ls-remote $(git rev-parse --abbrev-ref @{u} | sed 's/\// /g') | cut -f1) ]]; then
+    echo "Seems like you are not using the latest commit from self-hosted repository. Please pull the latest changes and try again.";
+    exit 1
+  fi
 fi
 
 source "$(dirname $0)/install/_lib.sh"  # does a `cd .../install/`, among other things
