4a56a7c2f48fad4b3c8540d58335999052d1b3ad
Amin Vakil <info@aminvakil.com>
2021-12-06 20:19:25 +0330

    Check for .git in parent folder

--- install/check-latest-commit.sh
+++ install/check-latest-commit.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Checks if we are on latest commit from github if it is running from master branch
-if [[ -d ".git" && "${NOT_LATEST_COMMIT:-0}" != 1 ]]; then
+if [[ -d "../.git" && "${NOT_LATEST_COMMIT:-0}" != 1 ]]; then
   if [[ $(git branch | sed -n '/\* /s///p') == "master" ]]; then
     if [[ $(git rev-parse HEAD) != $(git ls-remote $(git rev-parse --abbrev-ref @{u} | sed 's/\// /g') | cut -f1) ]]; then
       echo "Seems like you are not using the latest commit from self-hosted repository. Please pull the latest changes and try again.";
