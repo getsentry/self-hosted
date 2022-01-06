2b9a179270763a2919304fc0337fb2c307a499c0
Chad Whitacre <chadwhitacre@sentry.io>
2022-01-06 08:57:44 -0500

    Organize install.sh a bit

--- install.sh
+++ install.sh
@@ -1,20 +1,23 @@
 #!/usr/bin/env bash
 set -e
+
+# Pre-pre-flight? 🤷
 if [[ -n "$MSYSTEM" ]]; then
   echo "Seems like you are using an MSYS2-based system (such as Git Bash) which is not supported. Please use WSL instead.";
   exit 1
 fi
 
-umask 002
-
 source "$(dirname $0)/install/_lib.sh"  # does a `cd .../install/`, among other things
 
+# Pre-flight. No impact yet.
 source parse-cli.sh
 source dc-detect-version.sh
 source error-handling.sh
-source turn-things-off.sh
 source check-latest-commit.sh
 source check-minimum-requirements.sh
+
+# Let's go! Start impacting things.
+source turn-things-off.sh
 source create-docker-volumes.sh
 source ensure-files-from-examples.sh
 source generate-secret-key.sh
