dcd1e250307a76fba61c173aa8b9df3315ca7793
Chad Whitacre <chadwhitacre@sentry.io>
2022-02-01 15:04:01 -0500

    More parity with local

--- install/dc-detect-version.sh
+++ install/dc-detect-version.sh
@@ -2,6 +2,7 @@ echo "${_group}Initializing Docker Compose ..."
 
 # Some environments still use `docker-compose` even for Docker Compose v2.
 dc_base="$(docker compose version &> /dev/null && echo 'docker compose' || echo 'docker-compose')"
+dc_base="/Users/chadwhitacre/workbench/getsentry/self-hosted/docker-compose-1.28.0"
 if [[ "$(basename $0)" = "install.sh"  ]]; then
   dc="$dc_base --ansi never --env-file ${_ENV}"
 else
--- install/relay-credentials.sh
+++ install/relay-credentials.sh
@@ -2,17 +2,25 @@ echo "${_group}Generating Relay credentials ..."
 
 RELAY_CONFIG_YML="../relay/config.yml"
 RELAY_CREDENTIALS_JSON="../relay/credentials.json"
+RELAY_CREDENTIALS_JSON_TMP="/tmp/credentials.json"
 
 ensure_file_from_example $RELAY_CONFIG_YML
 
 if [[ -f "$RELAY_CREDENTIALS_JSON" ]]; then
   echo "$RELAY_CREDENTIALS_JSON already exists, skipped creation."
 else
-  ls -FGl ../relay
-  $dcr relay credentials generate
+
+  # If we call `docker compose run relay`
+
+  $dcr \
+    --no-deps \
+    --volume "$(pwd)/$RELAY_CONFIG_YML:/tmp/config.yml" \
+    relay --config /tmp credentials generate --stdout \
+    > "$RELAY_CREDENTIALS_JSON_TMP"
+
   ls -FGl ../relay
   cat ../relay/credentials.json
-  $dcr relay credentials show
+  $dcr relay credentials show 
   if [[ -f "$RELAY_CREDENTIALS_JSON" ]]; then
     echo "Relay credentials written to $RELAY_CREDENTIALS_JSON."
   else
