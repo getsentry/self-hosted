879f2384a8425fc976818bacb33740e32fff1b8b
John Bolliger <johnbolliger@gmail.com>
2022-02-20 03:44:38 -0700

    got sentry booting with redpanda

--- install/create-kafka-topics.sh
+++ install/create-kafka-topics.sh
@@ -1,12 +1,18 @@
 echo "${_group}Creating additional Kafka topics ..."
 
+# TODO: Migrate existing kafka topics over to redpanda syntax
+
 # NOTE: This step relies on `kafka` being available from the previous `snuba-api bootstrap` step
 # XXX(BYK): We cannot use auto.create.topics as Confluence and Apache hates it now (and makes it very hard to enable)
-EXISTING_KAFKA_TOPICS=$($dcr -T kafka kafka-topics --list --bootstrap-server kafka:9092 2>/dev/null)
+# EXISTING_KAFKA_TOPICS=$($dcr -T kafka kafka-topics --list --bootstrap-server kafka:9092 2>/dev/null)
+
+EXISTING_KAFKA_TOPICS=""
+echo $EXISTING_KAFKA_TOPICS
+
 NEEDED_KAFKA_TOPICS="ingest-attachments ingest-transactions ingest-events"
 for topic in $NEEDED_KAFKA_TOPICS; do
   if ! echo "$EXISTING_KAFKA_TOPICS" | grep -wq $topic; then
-    $dcr kafka kafka-topics --create --topic $topic --bootstrap-server kafka:9092
+    $dcr redpanda topic create $topic --brokers kafka:9092
     echo ""
   fi
 done
