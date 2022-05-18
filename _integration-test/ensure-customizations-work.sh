96848f4fe6c893240dc99913ffa6a28c30bd9355
Benedikt Franke <benedikt.franke@mll.com>
2022-05-18 19:00:23 +0200

    Fix integration test permissions and logic

--- _integration-test/ensure-customizations-work.sh
+++ _integration-test/ensure-customizations-work.sh
@@ -7,6 +7,6 @@ source ../install/dc-detect-version.sh
 
 # Negated version of ensure-customizations-not-present.sh, make changes in sync
 echo "${_group}Ensure customizations work"
-$dcr web bash -c "if [ ! -e /created-by-enhance-image ]; then exit 1; fi"
-$dcr web python -c "import ldap"
+$dcr --no-deps web bash -c "if [ ! -e /created-by-enhance-image ]; then exit 1; fi"
+$dcr --no-deps web python -c "import ldap"
 echo "${_endgroup}"
--- integration-test.sh
+++ integration-test.sh
@@ -1,6 +1,10 @@
 #!/usr/bin/env bash
 set -ex
 
+echo "Reset customizations"
+rm -f sentry/enhance-image.sh
+rm -f sentry/requirements.txt
+
 echo "Testing initial install"
 ./install.sh
 ./_integration-test/run.sh
@@ -13,6 +17,7 @@ touch /created-by-enhance-image
 apt-get update
 apt-get install -y gcc libsasl2-dev python-dev libldap2-dev libssl-dev
 EOT
+chmod +x sentry/enhance-image.sh
 printf "python-ldap" > sentry/requirements.txt
 
 echo "Testing in-place upgrade and customizations"
