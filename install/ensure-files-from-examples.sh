73ac8e39b77265af1aa27b0d1d9abe896a819cb0
Benedikt Franke <benedikt.franke@mll.com>
2022-05-18 11:54:07 +0200

    Use quotes around variable

--- install/ensure-files-from-examples.sh
+++ install/ensure-files-from-examples.sh
@@ -1,7 +1,7 @@
 echo "${_group}Ensuring files from examples ..."
 
-ensure_file_from_example $SENTRY_CONFIG_PY
-ensure_file_from_example $SENTRY_CONFIG_YML
+ensure_file_from_example "$SENTRY_CONFIG_PY"
+ensure_file_from_example "$SENTRY_CONFIG_YML"
 ensure_file_from_example '../symbolicator/config.yml'
 ensure_file_from_example '../sentry/enhance-image.sh'
 ensure_file_from_example '../sentry/requirements.txt'
