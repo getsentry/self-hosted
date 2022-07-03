a5f6aad8b769e03a057bbf9da0a8e9ccef502906
goganchic <goganchic@gmail.com>
2022-07-03 18:57:13 +0300

    remove unnecessary env variables passing

--- install/build-docker-images.sh
+++ install/build-docker-images.sh
@@ -3,8 +3,8 @@ echo "${_group}Building and tagging Docker images ..."
 echo ""
 # Build any service that provides the image sentry-self-hosted-local first,
 # as it is used as the base image for sentry-cleanup-self-hosted-local.
-$dc build --build-arg "http_proxy=${http_proxy:-}" --build-arg "https_proxy=${https_proxy:-}" --build-arg "no_proxy=${no_proxy:-}" --force-rm web
-$dc build --build-arg "http_proxy=${http_proxy:-}" --build-arg "https_proxy=${https_proxy:-}" --build-arg "no_proxy=${no_proxy:-}" --force-rm
+$dc build --force-rm web
+$dc build --force-rm
 echo ""
 echo "Docker images built."
 
