5c7b28d54fc735a2a1e657333041df4b8bd6a8ce
Ethan Smith <ethan.smith@sentry.io>
2022-09-01 12:54:59 -0700

    WIP 2

--- install.sh
+++ install.sh
@@ -1,32 +1,20 @@
 #!/usr/bin/env bash
 set -eE
 
-export SENTRY_DSN='https://5a620019b5124cbba230a9e62db9b825@o1.ingest.sentry.io/6627632'
-#export SENTRY_ORG=sentry-self-hosted
-#export SENTRY_PROJECT=dogfooding
+echo "Would you like to opt-in to error monitoring for the Sentry installer?"
+echo "This helps us catch and fix errors when installing Sentry."
+echo "We retain your OS name, installer log, IP, and username for 30 days."
+echo "Your information is solely used for error monitoring, we do not share your data."
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) export REPORT_ERRORS=1; break;;
+        No ) export REPORT_ERRORS=0; break;;
+    esac
+done
 
-function err_info {
-  local retcode=$?
-  local cmd="${BASH_COMMAND}"
-  if [[ $retcode -ne 0 ]]; then
-    set +o xtrace
-    echo "Error in ${BASH_SOURCE[0]}:${BASH_LINENO[0]}." >&2
-    echo "'$cmd' exited with status $retcode" >&2
-    local stack_depth=${#FUNCNAME[@]}
-    if [ $stack_depth -gt 2 ]; then
-      for ((i=$(($stack_depth - 1)),j=1;i>0;i--,j++)); do
-          local indent="$(yes a | head -$j | tr -d '\n')"
-          local src=${BASH_SOURCE[$i]}
-          local lineno=${BASH_LINENO[$i-1]}
-          local funcname=${FUNCNAME[$i]}
-          echo "${indent//a/-}>$src:$funcname:$lineno" >&2
-      done
-    fi
-  fi
-  echo "Exiting with code $retcode" >&2
-  exit $retcode
-}
-trap 'err_info' EXIT
+if [ "$REPORT_ERRORS" == 1 ]; then
+  trap 'err_info' EXIT
+fi
 
 # Pre-pre-flight? 🤷
 if [[ -n "$MSYSTEM" ]]; then
@@ -34,30 +22,34 @@ if [[ -n "$MSYSTEM" ]]; then
   exit 1
 fi
 
-source "$(dirname $0)/install/_lib.sh"  # does a `cd .../install/`, among other things
+function main() {
+  source "$(dirname $0)/install/_lib.sh"  # does a `cd .../install/`, among other things
 
-# Pre-flight. No impact yet.
-source parse-cli.sh
-source dc-detect-version.sh
-source error-handling.sh
-source check-latest-commit.sh
-source check-minimum-requirements.sh
+  # Pre-flight. No impact yet.
+  source parse-cli.sh
+  source dc-detect-version.sh
+  source error-handling.sh
+  source check-latest-commit.sh
+  source check-minimum-requirements.sh
 
-# Let's go! Start impacting things.
-source turn-things-off.sh
-source create-docker-volumes.sh
-source ensure-files-from-examples.sh
-source ensure-relay-credentials.sh
-source generate-secret-key.sh
-source replace-tsdb.sh
-source update-docker-images.sh
-source build-docker-images.sh
-source set-up-zookeeper.sh
-source install-wal2json.sh
-source bootstrap-snuba.sh
-source create-kafka-topics.sh
-source upgrade-postgres.sh
-source set-up-and-migrate-database.sh
-source migrate-file-storage.sh
-source geoip.sh
-source wrap-up.sh
+  # Let's go! Start impacting things.
+  source turn-things-off.sh
+  source create-docker-volumes.sh
+  source ensure-files-from-examples.sh
+  source ensure-relay-credentials.sh
+  source generate-secret-key.sh
+  source replace-tsdb.sh
+  source update-docker-images.sh
+  source build-docker-images.sh
+  source set-up-zookeeper.sh
+  source install-wal2json.sh
+  source bootstrap-snuba.sh
+  source create-kafka-topics.sh
+  source upgrade-postgres.sh
+  source set-up-and-migrate-database.sh
+  source migrate-file-storage.sh
+  source geoip.sh
+  source wrap-up.sh
+}
+
+main
--- install/error-handling.sh
+++ install/error-handling.sh
@@ -1,5 +1,31 @@
 echo "${_group}Setting up error handling ..."
 
+export SENTRY_DSN='https://afa81b4b7c634e318b0ec138abb2645f@self-hosted.getsentry.net/2'
+export SENTRY_ORG=sentry
+export SENTRY_PROJECT=dogfooding
+
+function err_info {
+  local retcode=$?
+  local cmd="${BASH_COMMAND}"
+  if [[ $retcode -ne 0 ]]; then
+    set +o xtrace
+    echo "Error in ${BASH_SOURCE[0]}:${BASH_LINENO[0]}." >&2
+    echo "'$cmd' exited with status $retcode" >&2
+    local stack_depth=${#FUNCNAME[@]}
+    if [ $stack_depth -gt 2 ]; then
+      for ((i=$(($stack_depth - 1)),j=1;i>0;i--,j++)); do
+          local indent="$(yes a | head -$j | tr -d '\n')"
+          local src=${BASH_SOURCE[$i]}
+          local lineno=${BASH_LINENO[$i-1]}
+          local funcname=${FUNCNAME[$i]}
+          echo "${indent//a/-}>$src:$funcname:$lineno" >&2
+      done
+    fi
+  fi
+  echo "Exiting with code $retcode" >&2
+  exit $retcode
+}
+
 # Courtesy of https://stackoverflow.com/a/2183063/90297
 trap_with_arg() {
   func="$1" ; shift
