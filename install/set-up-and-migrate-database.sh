9df7e7da3001ae980a6dd77a1d18068a8ffc027c
Chad Whitacre <chadwhitacre@sentry.io>
2022-09-14 15:59:12 -0400

    Rename flag to --skip-user-creation (#1696)

--- install/set-up-and-migrate-database.sh
+++ install/set-up-and-migrate-database.sh
@@ -1,10 +1,10 @@
 echo "${_group}Setting up / migrating database ..."
 
-if [[ -n "${CI:-}" || "${SKIP_USER_PROMPT:-0}" == 1 ]]; then
+if [[ -n "${CI:-}" || "${SKIP_USER_CREATION:-0}" == 1 ]]; then
   $dcr web upgrade --noinput
   echo ""
-  echo "Did not prompt for user creation due to non-interactive shell."
-  echo "Run the following command to create one yourself (recommended):"
+  echo "Did not prompt for user creation. Run the following command to create one"
+  echo "yourself (recommended):"
   echo ""
   echo "  $dc_base run --rm web createuser"
   echo ""
