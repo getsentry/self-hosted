e59fce01477f62c326bb24674ceb6e509dbc0ef8
Chad Whitacre <chadwhitacre@sentry.io>
2022-09-14 15:39:32 -0400

    Make message more honest

--- install/set-up-and-migrate-database.sh
+++ install/set-up-and-migrate-database.sh
@@ -3,8 +3,8 @@ echo "${_group}Setting up / migrating database ..."
 if [[ -n "${CI:-}" || "${SKIP_USER_CREATION:-0}" == 1 ]]; then
   $dcr web upgrade --noinput
   echo ""
-  echo "Did not prompt for user creation due to non-interactive shell."
-  echo "Run the following command to create one yourself (recommended):"
+  echo "Did not prompt for user creation. Run the following command to create one"
+  echo "yourself (recommended):"
   echo ""
   echo "  $dc_base run --rm web createuser"
   echo ""
