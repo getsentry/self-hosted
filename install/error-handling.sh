7fbeff359aa0248abb215abd3b89cbeff8a79fd2
Ethan Smith <ethan.smith@sentry.io>
2022-09-19 11:02:50 -0700

    WIP log attachment

--- install/error-handling.sh
+++ install/error-handling.sh
@@ -11,6 +11,35 @@ function send_event {
   command popd > /dev/null
 }
 
+
+function create_envelope() {
+  local cli_output=$1;
+  # Get the UUID of event
+  local event_fingerprint=${cli_output#Event dispatched: };
+  # The event_id is the UUID without dashes
+  local event_id=${event_fingerprint//-/};
+  local envelope_file="/tmp/sentry-envelope-${event_id}"
+  # If the envelope file exists, we've already sent it, so we are done
+  if [[ -f envelope_file ]]; then
+    return;
+  fi
+  # If we haven't sent the envelope file, make it and send to Sentry
+  # The format is documented at https://develop.sentry.dev/sdk/envelopes/
+  # Get length of file, needed for the envelope header
+  local file_length=$(wc -c < $log_file);
+  echo $(jq -n \
+    --arg event_id "$event_id" \
+    --arg dsn "$SENTRY_DSN" \
+    '{"event_id": $event_id, "dsn": $dsn}' \
+  ) > $envelope_file;
+  echo $(jq -n \
+    --arg length "$file_length" \
+    --arg filename "$log_file" \
+    '{"type": "attachment", "length": $length, "content_type": "text/plain", "filename": $filename}' \
+  ) >> $envelope_file;
+  cat $log_file >> $envelope_file;
+}
+
 if [[ -z "${REPORT_SELF_HOSTED_ISSUES:-}" ]]; then
   if [[ $PROMPTABLE == "0" ]]; then
     echo
@@ -149,7 +178,8 @@ cleanup () {
 
     if [ "$REPORT_SELF_HOSTED_ISSUES" == 1 ]; then
       local traceback_hash=$(echo -n $traceback | docker run -i --rm busybox md5sum | cut -d' ' -f1)
-      send_event "$traceback_hash" "$cmd_exit"
+      local cli_output=$(send_event "$traceback_hash" "$cmd_exit")
+      create_envelope $cli_output
     fi
 
     if [[ -n "$MINIMIZE_DOWNTIME" ]]; then
