a7c9f8938ddf32693f7e1da2051dfcf1a3f11b64
hubertdeng123 <hubertdeng123@gmail.com>
2022-09-29 12:00:41 -0700

    preserve semantics from docker volume test

--- install/create-docker-volumes-test.sh
+++ install/create-docker-volumes-test.sh
@@ -1,23 +1,30 @@
 #!/usr/bin/env bash
 source "$(dirname $0)/_test_setup.sh"
 
-expected_volumes="
-sentry-clickhouse
+get_volumes() {
+  # If grep returns no strings, we still want to return without error
+  docker volume ls --quiet | { grep '^sentry-.*' || true; } | sort
+}
+
+# Maybe they exist prior, maybe they don't. Script is idempotent.
+
+expected_volumes="sentry-clickhouse
 sentry-data
 sentry-kafka
 sentry-postgres
 sentry-redis
 sentry-symbolicator
-sentry-zookeeper
-"
+sentry-zookeeper"
+
+before=$(get_volumes)
+
+test "$before" == "" || test "$before" == $expected_volumes
 
 source create-docker-volumes.sh
 source create-docker-volumes.sh
 source create-docker-volumes.sh
 
-docker_volumes=$(docker volume ls --quiet | grep '^sentry-.*')
-for expected_volume in $expected_volumes; do
-  [[ "$docker_volumes" =~ "$expected_volume" ]] || exit 1
-done
+after=$(get_volumes)
+test "$after" == "$expected_volumes"
 
 report_success
