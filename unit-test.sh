fa1f96def070204c7824e7ea4a338d21c555f147
hubertdeng123 <hubertdeng123@gmail.com>
2022-09-28 17:06:40 -0700

    move unit tests to unit test directory

--- unit-test.sh
+++ unit-test.sh
@@ -1,6 +1,8 @@
 #!/usr/bin/env bash
 fail=0
-for test_file in ./install/*-test.sh; do
+# -perm +111 finds files with the executable bit set
+test_files=$(find ./_unit-test/ -type f -perm +111)
+for test_file in $test_files; do
     $test_file
     if [ $? != 0 ]; then
         fail=1
