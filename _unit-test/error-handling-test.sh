2ee767d42a394c245d8a51d1917b91aed4e86f77
Benedikt Franke <benedikt.franke@mll.com>
2022-11-08 14:29:35 +0100

    Fix log_path -> log_file

--- _unit-test/error-handling-test.sh
+++ _unit-test/error-handling-test.sh
@@ -14,12 +14,12 @@ send_envelope() {
 
 export -f send_envelope
 echo "Testing initial send_event"
-export log_path="$PROJECT_ROOT/test_log.txt"
-echo "Test Logs" >"$log_path"
-echo "Error Msg" >>"$log_path"
+export log_file="$PROJECT_ROOT/test_log.txt"
+echo "Test Logs" >"$log_file"
+echo "Error Msg" >>"$log_file"
 breadcrumbs=$(generate_breadcrumb_json | sed '$d' | jq -s -c)
 SEND_EVENT_RESPONSE=$(send_event "12345123451234512345123451234512" "Test exited with status 1" "{\"ignore\": \"me\"}" "$breadcrumbs")
-rm "$log_path"
+rm "$log_file"
 test "$SEND_EVENT_RESPONSE" == 'Test Sending sentry-envelope-12345123451234512345123451234512'
 ENVELOPE_CONTENTS=$(cat /tmp/sentry-envelope-12345123451234512345123451234512)
 test "$ENVELOPE_CONTENTS" == "$(cat "$PROJECT_ROOT/_unit-test/snapshots/sentry-envelope-12345123451234512345123451234512")"
@@ -35,9 +35,9 @@ echo "Testing cleanup without minimizing downtime"
 export REPORT_SELF_HOSTED_ISSUES=0
 export MINIMIZE_DOWNTIME=''
 export dc=':'
-echo "Test Logs" >"$log_path"
+echo "Test Logs" >"$log_file"
 CLEANUP_RESPONSE=$(cleanup ERROR)
-rm "$log_path"
+rm "$log_file"
 test "$CLEANUP_RESPONSE" == 'Error in ./_unit-test/error-handling-test.sh:37.
 '\''local cmd="${BASH_COMMAND}"'\'' exited with status 0
 
@@ -47,9 +47,9 @@ echo "Pass."
 echo "Testing cleanup while minimizing downtime"
 export REPORT_SELF_HOSTED_ISSUES=0
 export MINIMIZE_DOWNTIME=1
-echo "Test Logs" >"$log_path"
+echo "Test Logs" >"$log_file"
 CLEANUP_RESPONSE=$(cleanup ERROR)
-rm "$log_path"
+rm "$log_file"
 test "$CLEANUP_RESPONSE" == 'Error in ./_unit-test/error-handling-test.sh:49.
 '\''local cmd="${BASH_COMMAND}"'\'' exited with status 0
 
