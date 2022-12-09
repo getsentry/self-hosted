97fbc5ba79de95ee74d397230be73adf990404f5
Ethan Smith <ethan.smith@sentry.io>
2022-12-08 17:39:44 -0800

    Switch to docker compose --services for building each service one at a time

--- install/build-docker-images.sh
+++ install/build-docker-images.sh
@@ -1,23 +1,11 @@
 echo "${_group}Building and tagging Docker images ..."
 
-declare -a SERVICES=("cron"
-                     "worker"
-                     "ingest-consumer"
-                     "post-process-forwarder-errors"
-                     "post-process-forwarder-transactions"
-                     "subscription-consumer-events"
-                     "subscription-consumer-transactions"
-                     "clickhouse"
-                     "snuba-cleanup"
-                     "snuba-transactions-cleanup"
-                     "symbolicator-cleanup"
-                     "sentry-cleanup")
 
 echo ""
 # Build any service that provides the image sentry-self-hosted-local first,
 # as it is used as the base image for sentry-cleanup-self-hosted-local.
 $dc build --build-arg "http_proxy=${http_proxy:-}" --build-arg "https_proxy=${https_proxy:-}" --build-arg "no_proxy=${no_proxy:-}" --force-rm web
-for service in "${SERVICES[@]}"
+for service in "$($dc config --services)"
 do
    $dc build --build-arg "http_proxy=${http_proxy:-}" --build-arg "https_proxy=${https_proxy:-}" --build-arg "no_proxy=${no_proxy:-}" --force-rm $service
 done
