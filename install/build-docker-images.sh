6b09d0aad1886386196217d5d96251d878b6e473
Ethan Smith <ethan.smith@sentry.io>
2022-12-09 11:00:48 -0800

    Build each service image individually (#1858)
    Fixes https://github.com/getsentry/self-hosted/issues/1847

--- install/build-docker-images.sh
+++ install/build-docker-images.sh
@@ -4,7 +4,9 @@ echo ""
 # Build any service that provides the image sentry-self-hosted-local first,
 # as it is used as the base image for sentry-cleanup-self-hosted-local.
 $dc build --build-arg "http_proxy=${http_proxy:-}" --build-arg "https_proxy=${https_proxy:-}" --build-arg "no_proxy=${no_proxy:-}" --force-rm web
-$dc build --build-arg "http_proxy=${http_proxy:-}" --build-arg "https_proxy=${https_proxy:-}" --build-arg "no_proxy=${no_proxy:-}" --force-rm
+for service in "$($dc config --services)"; do
+  $dc build --build-arg "http_proxy=${http_proxy:-}" --build-arg "https_proxy=${https_proxy:-}" --build-arg "no_proxy=${no_proxy:-}" --force-rm $service
+done
 # Used in error-handling.sh for error envelope payloads
 docker build -t sentry-self-hosted-jq-local --platform=$DOCKER_PLATFORM $basedir/jq
 echo ""
