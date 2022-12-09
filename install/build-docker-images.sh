066bd1788e4760efb8efe738928260ad3c3a8062
Ethan Smith <ethan.smith@sentry.io>
2022-12-08 17:43:51 -0800

    Fix indent

--- install/build-docker-images.sh
+++ install/build-docker-images.sh
@@ -5,7 +5,7 @@ echo ""
 # as it is used as the base image for sentry-cleanup-self-hosted-local.
 $dc build --build-arg "http_proxy=${http_proxy:-}" --build-arg "https_proxy=${https_proxy:-}" --build-arg "no_proxy=${no_proxy:-}" --force-rm web
 for service in "$($dc config --services)"; do
-   $dc build --build-arg "http_proxy=${http_proxy:-}" --build-arg "https_proxy=${https_proxy:-}" --build-arg "no_proxy=${no_proxy:-}" --force-rm $service
+  $dc build --build-arg "http_proxy=${http_proxy:-}" --build-arg "https_proxy=${https_proxy:-}" --build-arg "no_proxy=${no_proxy:-}" --force-rm $service
 done
 # Used in error-handling.sh for error envelope payloads
 docker build -t sentry-self-hosted-jq-local --platform=$DOCKER_PLATFORM $basedir/jq
