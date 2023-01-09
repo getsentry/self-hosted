4fc46b405f912703aadd4910bb55517fef45fa00
Chad Whitacre <chadwhitacre@sentry.io>
2023-01-09 15:01:33 -0500

    Add wal2json debugging

--- install/install-wal2json.sh
+++ install/install-wal2json.sh
@@ -16,11 +16,11 @@ docker_curl() {
 }
 
 if [[ $WAL2JSON_VERSION == "latest" ]]; then
-  VERSION=$(
-    docker_curl https://api.github.com/repos/getsentry/wal2json/releases/latest |
-      grep '"tag_name":' |
-      sed -E 's/.*"([^"]+)".*/\1/'
-  )
+  docker_curl https://api.github.com/repos/getsentry/wal2json/releases/latest > wal2json.json
+  cat "====================================================="
+  cat wal2json.son
+  cat "====================================================="
+  VERSION=$(grep '"tag_name":' wal2json.json | sed -E 's/.*"([^"]+)".*/\1/')
   if [[ ! $VERSION ]]; then
     echo "Cannot find wal2json latest version"
     exit 1
