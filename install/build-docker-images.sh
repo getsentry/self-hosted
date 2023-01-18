84adc234ee5207477f51cd87a583cc50082e4a74
Benedikt Franke <benedikt.franke@mll.com>
2023-01-18 10:03:11 +0100

    avoid unnecessary loop

--- install/build-docker-images.sh
+++ install/build-docker-images.sh
@@ -4,9 +4,7 @@ echo ""
 # Build any service that provides the image sentry-self-hosted-local first,
 # as it is used as the base image for sentry-cleanup-self-hosted-local.
 $dcb --force-rm web
-for service in "$($dc config --services)"; do
-  $dcb --force-rm "$service"
-done
+$dcb --force-rm $($dc config --services)
 echo ""
 echo "Docker images built."
 
