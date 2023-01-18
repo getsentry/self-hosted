1f0faa36ba9d5fd0692e87275144e3e71af1171b
Ethan Smith <ethan.smith@sentry.io>
2023-01-18 13:26:42 -0800

    Revert "Try killing symbolicator to see how it looks in failure mode"
    This reverts commit b8959016bf8164014dd9e6e01d2ca14dfc26c982.

--- _integration-test/run.sh
+++ _integration-test/run.sh
@@ -131,7 +131,6 @@ echo '------------------------------------------'
 echo "${_endgroup}"
 
 echo "${_group}Test symbolicator works ..."
-$dc stop symbolicator
 SENTRY_ORG="${SENTRY_ORG:-sentry}"
 SENTRY_PROJECT="${SENTRY_PROJECT:-native}"
 SENTRY_TEAM="${SENTRY_TEAM:-sentry}"
