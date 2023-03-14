66181e0e8db25d24d53f987ecd7ac9925a75c0ed
hubertdeng123 <hubertdeng123@gmail.com>
2023-03-14 16:26:32 -0700

    add opt in error monitoring to reset and clean scripts

--- reset.sh
+++ reset.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-./clean.sh "$1"
-./install.sh
+# Needed variable to source install script
+MSYSTEM="${MSYSTEM:-}"
+log_name=reset
+
+source set-up-error-reporting-for-scripts.sh
+source docker_cleanup.sh
+source install.sh
