e177e8cb32cfd407b42522f7bddae8e0539fea87
hubertdeng123 <hubertdeng123@gmail.com>
2023-03-15 16:59:27 -0700

    remove reset script

--- reset.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-# Needed variable to source install script
-MSYSTEM="${MSYSTEM:-}"
-log_name=reset
-
-source set-up-error-reporting-for-scripts.sh
-source docker-cleanup.sh
-source install.sh
