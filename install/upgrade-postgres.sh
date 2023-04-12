a6d46a900bdde6b7485681e11aade258901e8819
Hubert Deng <hubert.deng@sentry.io>
2023-04-11 17:15:13 -0700

    Upgrade Postgres to 14 (#2074)
    * add postgres upgrade script, this is included in the install script and automatically upgrades users from PG 9.6 to 14

---
+++ install/upgrade-postgres.sh
@@ -0,0 +1,44 @@
+echo "${_group}Ensuring proper PostgreSQL version ..."
+
+if [[ -n "$(docker volume ls -q --filter name=sentry-postgres)" && "$(docker run --rm -v sentry-postgres:/db busybox cat /db/PG_VERSION 2>/dev/null)" == "9.6" ]]; then
+  docker volume rm sentry-postgres-new || true
+  # If this is Postgres 9.6 data, start upgrading it to 14.0 in a new volume
+  docker run --rm \
+    -v sentry-postgres:/var/lib/postgresql/9.6/data \
+    -v sentry-postgres-new:/var/lib/postgresql/14/data \
+    tianon/postgres-upgrade:9.6-to-14
+
+  # Get rid of the old volume as we'll rename the new one to that
+  docker volume rm sentry-postgres
+  docker volume create --name sentry-postgres
+  # There's no rename volume in Docker so copy the contents from old to new name
+  # Also append the `host all all all trust` line as `tianon/postgres-upgrade:9.6-to-14`
+  # doesn't do that automatically.
+  docker run --rm -v sentry-postgres-new:/from -v sentry-postgres:/to alpine ash -c \
+    "cd /from ; cp -av . /to ; echo 'host all all all trust' >> /to/pg_hba.conf"
+  # Finally, remove the new old volume as we are all in sentry-postgres now.
+  docker volume rm sentry-postgres-new
+  echo "Re-indexing due to glibc change, this may take a while..."
+  echo "Starting up new PostgreSQL version"
+  $dc up -d postgres
+
+  # Wait for postgres
+  RETRIES=5
+  until docker exec sentry-self-hosted-postgres-1 psql -U postgres -c "select 1" >/dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+    echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
+    sleep 1
+  done
+
+  # VOLUME_NAME is the same as container name
+  # Reindex all databases and their system catalogs which are not templates
+  DBS=$(docker exec sentry-self-hosted-postgres-1 psql -qAt -U postgres -c "SELECT datname FROM pg_database WHERE datistemplate = false;")
+  for db in ${DBS}; do
+    echo "Re-indexing database: ${db}"
+    docker exec sentry-self-hosted-postgres-1 psql -qAt -U postgres -d ${db} -c "reindex system ${db}"
+    docker exec sentry-self-hosted-postgres-1 psql -qAt -U postgres -d ${db} -c "reindex database ${db};"
+  done
+
+  $dc stop postgres
+fi
+
+echo "${_endgroup}"
