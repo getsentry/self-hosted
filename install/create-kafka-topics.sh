55441d055c03d262f78c8b81e3ed33cd39c71657
hubertdeng123 <hubertdeng123@gmail.com>
2023-06-21 15:57:19 -0700

    don't even check if existing kafka topic exists before creating

--- install/create-kafka-topics.sh
+++ install/create-kafka-topics.sh
@@ -2,13 +2,10 @@ echo "${_group}Creating additional Kafka topics ..."
 
 # NOTE: This step relies on `kafka` being available from the previous `snuba-api bootstrap` step
 # XXX(BYK): We cannot use auto.create.topics as Confluence and Apache hates it now (and makes it very hard to enable)
-EXISTING_KAFKA_TOPICS=$($dcr -T kafka kafka-topics --list --bootstrap-server kafka:9092 2>/dev/null)
 NEEDED_KAFKA_TOPICS="ingest-attachments ingest-transactions ingest-events ingest-replay-recordings profiles"
 for topic in $NEEDED_KAFKA_TOPICS; do
-  if ! echo "$EXISTING_KAFKA_TOPICS" | grep -wq $topic; then
-    $dcr kafka kafka-topics --create --topic $topic --bootstrap-server kafka:9092
-    echo ""
-  fi
+  $dcr kafka kafka-topics --create --topic $topic --bootstrap-server kafka:9092
+  echo ""
 done
 
 echo "${_endgroup}"
