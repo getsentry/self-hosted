728c5dd6e045de921ccb38e2be79e07df6dbf687
Chad Whitacre <chadwhitacre@sentry.io>
2023-11-17 10:41:55 -0500

    Relicense under FSL-1.0-Apache-2.0

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -9,7 +9,6 @@ WAL2JSON_VERSION=${WAL2JSON_VERSION:-$(curl -s "https://api.github.com/repos/get
 sed -i -e "s/^WAL2JSON_VERSION=\([^:]\+\):.\+\$/WAL2JSON_VERSION=\1:$WAL2JSON_VERSION/" .env
 sed -i -e "s/^\(SENTRY\|SNUBA\|RELAY\|SYMBOLICATOR\|VROOM\)_IMAGE=\([^:]\+\):.\+\$/\1_IMAGE=\2:$NEW_VERSION/" .env
 sed -i -e "s/^\# Self-Hosted Sentry .*/# Self-Hosted Sentry $NEW_VERSION/" README.md
-sed -i -e "s/\(Change Date:\s*\)[-0-9]\+\$/\\1$(date +'%Y-%m-%d' -d '3 years')/" LICENSE
 
 echo "New version: $NEW_VERSION"
 echo "New wal2json version: $WAL2JSON_VERSION"
