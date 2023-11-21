efa2901982c95a1efe61c965381242d2afc6ccee
Alex Zaslavsky <azaslavsky@users.noreply.github.com>
2023-11-21 11:47:30 -0800

    Remove duplicate backup tests

--- _integration-test/ensure-sentry-admin-works.sh
+++ _integration-test/ensure-sentry-admin-works.sh
@@ -25,26 +25,3 @@ if ! echo "$command_help_doc" | grep -q "^Usage: ./sentry-admin.sh permissions";
   echo "Assertion failed: Incorrect binary name in command-specific help docs"
   exit 1
 fi
-
-echo "Exports via sentry-admin..."
-
-# Docker was giving me permissioning issues when trying to create this file and write to it even
-# after giving read + write access to group and owner. Instead, try creating the empty file and then
-# give everyone write access to the backup file
-touch $(pwd)/sentry-admin/backup.json
-chmod 666 $(pwd)/sentry-admin/backup.json
-
-# This command uses the `sentry-admin.sh` script to try an admin command.
-touch $(pwd)/sentry/admin/backup.json
-chmod -R 666 $(pwd)/sentry/admin/backup.json
-SENTRY_DOCKER_IO_DIR=$(pwd)/sentry-admin /bin/bash $(pwd)/sentry-admin.sh export global /sentry-admin/backup.json
-if [ ! -s "$(pwd)/sentry/backup.json" ]; then
-  echo "Assertion failed: Backup file is empty"
-  exit 1
-fi
-
-# Print backup.json contents
-echo "Backup file contents:\n\n"
-cat "$(pwd)/sentry-admin/backup.json"
-
-rm $(pwd)/sentry-admin/backup.json
