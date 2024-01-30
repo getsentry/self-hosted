b175988566e75a85bdf34a6bc995e7a427270f7e
ngudbhav <ngudbhav05@hotmail.com>
2024-01-30 12:20:03 +0530

    fix: workaround to correctly export ENV variables

--- .platform/hooks/prebuild/01_prebuild.sh
+++ .platform/hooks/prebuild/01_prebuild.sh
@@ -3,7 +3,9 @@
 set -e
 /opt/elasticbeanstalk/bin/get-config environment | jq -r 'to_entries | .[] | "\(.key)=\"\(.value)\""' > .env
 printf "SENTRY_IMAGE=getsentry/sentry:23.12.1" >> .env
-export $(cat .env | xargs)
+set -a
+source .env
+set +a
 docker image prune -f
 ./install.sh --no-user-prompt --no-report-self-hosted-issues
 wait
--- .platform/hooks/predeploy/01_loadenv.sh
+++ .platform/hooks/predeploy/01_loadenv.sh
@@ -3,5 +3,7 @@
 set -e
 /opt/elasticbeanstalk/bin/get-config environment | jq -r 'to_entries | .[] | "\(.key)=\"\(.value)\""' > .env
 printf "SENTRY_IMAGE=getsentry/sentry:23.12.1" >> .env
-export $(cat .env | xargs)
+set -a
+source .env
+set +a
 wait
