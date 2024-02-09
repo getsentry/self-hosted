c3da53d2e76fa1773c2b5135b75344e3a57f6f19
Hubert Deng <hubert.deng@sentry.io>
2024-02-09 09:20:06 -0800

    fix: DB migration script (#2779)
    * use different approach to wait for postgres server

--- install/set-up-and-migrate-database.sh
+++ install/set-up-and-migrate-database.sh
@@ -2,10 +2,15 @@ echo "${_group}Setting up / migrating database ..."
 
 # Fixes https://github.com/getsentry/self-hosted/issues/2758, where a migration fails due to indexing issue
 $dc up -d postgres
-timeout 90s bash -c "until $dc exec postgres pg_isready ; do sleep 5 ; done"
+# Wait for postgres
+RETRIES=5
+until $dc exec postgres psql -U postgres -c "select 1" >/dev/null 2>&1 || [ $RETRIES -eq 0 ]; do
+  echo "Waiting for postgres server, $((RETRIES--)) remaining attempts..."
+  sleep 1
+done
 indexes=$($dc exec postgres psql -qAt -U postgres -c "SELECT indexname, indexdef FROM pg_indexes WHERE tablename = 'sentry_groupedmessage';")
 if [[ $indexes == *"sentry_groupedmessage_project_id_id_515aaa7e_uniq"* ]]; then
-  $dc postgres psql -qAt -U postgres -c "DROP INDEX sentry_groupedmessage_project_id_id_515aaa7e_uniq;"
+  $dc exec postgres psql -qAt -U postgres -c "DROP INDEX sentry_groupedmessage_project_id_id_515aaa7e_uniq;"
 fi
 
 if [[ -n "${CI:-}" || "${SKIP_USER_CREATION:-0}" == 1 ]]; then
