2f5e5c5b5b5ede0e1bd3918814c6259e05e508a3
Iven Schlenther <stossenhd@gmail.com>
2024-02-28 20:20:00 +0100

    simplify if for open-ai-suggestion (#2732)
    * simplify if for open-ai-suggestion

--- sentry/sentry.conf.example.py
+++ sentry/sentry.conf.example.py
@@ -317,8 +317,7 @@ GEOIP_PATH_MMDB = '/geoip/GeoLite2-City.mmdb'
 # OpenAI API key to turn on the feature.
 OPENAI_API_KEY = env("OPENAI_API_KEY", "")
 
-if OPENAI_API_KEY:
-  SENTRY_FEATURES["organizations:open-ai-suggestion"] = True
+SENTRY_FEATURES["organizations:open-ai-suggestion"] = bool(OPENAI_API_KEY)
 
 ##############################################
 # Content Security Policy settings
