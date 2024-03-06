07c059e46ebaaf75fd64a78e8d465d8d0cf101d3
hubertdeng123 <hubertdeng123@gmail.com>
2024-03-06 11:42:56 -0800

    fix sentry send event

--- _integration-test/run.sh
+++ _integration-test/run.sh
@@ -28,11 +28,12 @@ teardown() {
   DID_TEAR_DOWN=1
 
   if [ "$1" != "EXIT" ]; then
+    set +x
     error_msg="An error occurred, caught SIG$1 on line $2"
     echo "$error_msg"
     dsn="https://5a620019b5124cbba230a9e62db9b825@o1.ingest.us.sentry.io/6627632"
-    sentry_cli="docker run --rm -v /tmp:/work -e SENTRY_DSN=$dsn getsentry/sentry-cli"
-    $sentry_cli send-event -m "$error_msg" --logfile "$log_file"
+    local sentry_cli="docker run --rm -v $(pwd):/work -e SENTRY_DSN=$dsn getsentry/sentry-cli"
+    $sentry_cli send-event -m "$error_msg" --logfile $log_file
   fi
 
   echo "Tearing down ..."
