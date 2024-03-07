7559e557b3607817bc0525893dcdfdf7ef22bcf6
hubertdeng123 <hubertdeng123@gmail.com>
2024-03-07 14:26:16 -0800

    add error handling to test.sh

--- test.sh
+++ test.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -e
 
-# This file runs in https://github.com/getsentry/sentry/blob/fe4795f5eae9e0d7c33e0ecb736c9d1369535eca/docker/cloudbuild.yaml#L59
+export MINIMIZE_DOWNTIME=0
+export REPORT_SELF_HOSTED_ISSUES=1
 
-_integration-test/run.sh
+# This file runs in https://github.com/getsentry/sentry/blob/fe4795f5eae9e0d7c33e0ecb736c9d1369535eca/docker/cloudbuild.yaml#L59
+source install/_lib.sh
+source install/detect-platform.sh
+source install/dc-detect-version.sh
+source install/error-handling.sh
+source _integration-test/run.sh
