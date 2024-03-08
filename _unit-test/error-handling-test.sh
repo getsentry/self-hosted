17cf0cfbc2db159bff6c90a8d08474d1539e633c
hubertdeng123 <hubertdeng123@gmail.com>
2024-03-08 14:22:53 -0800

    update lineno

--- _unit-test/error-handling-test.sh
+++ _unit-test/error-handling-test.sh
@@ -55,7 +55,8 @@ export dc=':'
 echo "Test Logs" >"$log_file"
 CLEANUP_RESPONSE=$(cleanup ERROR) # the linenumber of this line must match just below
 rm "$log_file"
-test "$CLEANUP_RESPONSE" == 'Error in _unit-test/error-handling-test.sh:62.
+echo $CLEANUP_RESPONSE
+test "$CLEANUP_RESPONSE" == 'Error in _unit-test/error-handling-test.sh:56.
 '\''local cmd="${BASH_COMMAND}"'\'' exited with status 0
 
 Cleaning up...'
@@ -69,7 +70,7 @@ export MINIMIZE_DOWNTIME=1
 echo "Test Logs" >"$log_file"
 CLEANUP_RESPONSE=$(cleanup ERROR) # the linenumber of this line must match just below
 rm "$log_file"
-test "$CLEANUP_RESPONSE" == 'Error in _unit-test/error-handling-test.sh:76.
+test "$CLEANUP_RESPONSE" == 'Error in _unit-test/error-handling-test.sh:71.
 '\''local cmd="${BASH_COMMAND}"'\'' exited with status 0
 
 *NOT* cleaning up, to clean your environment run "docker compose stop".'
