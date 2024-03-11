57a3a207e3b905d73fbceedcfbb8b42f65137f24
Pierre Massat <pierre.massat@sentry.io>
2024-03-11 12:42:08 -0400

    Try to understand what's returned

--- _integration-test/run.sh
+++ _integration-test/run.sh
@@ -145,7 +145,7 @@ curl -sf --data-binary @$PROFILE_FIXTURE_PATH -H 'Content-Type: application/x-se
 printf "Getting the test profile back"
 PROFILE_ID="$(jq -r -n --slurpfile profile $PROFILE_FIXTURE_PATH '$profile[4].event_id')"
 PROFILE_PATH="projects/sentry/sentry/profiling/raw_profiles/$PROFILE_ID/"
-timeout 60 bash -c 'until $(sentry_api_request "$PROFILE_PATH" -Isf -X GET -o /dev/null); do printf '.'; sleep 0.5; done'
+timeout 60 bash -c 'until $(sentry_api_request "$PROFILE_PATH" -X GET -o /dev/null); do printf '.'; sleep 0.5; done'
 echo " got it!"
 echo "${_endgroup}"
 
@@ -157,7 +157,9 @@ curl -sf --data-binary @$PROFILE_FIXTURE_PATH -H 'Content-Type: application/x-se
 printf "Getting a span back"
 TRACE_ID="$(jq -r -n --slurpfile span $SPAN_FIXTURE_PATH '$span[2].contexts.trace.trace_id')"
 SPAN_PATH="organizations/sentry/events/?dataset=spansIndexed&field=id&project=1&query=trace%3A$TRACE_ID&statsPeriod=1h"
-timeout 60 bash -c 'until $(sentry_api_request "$SPAN_PATH" -sf -X GET | jq '.data[]' -e); do printf '.'; sleep 0.5; done'
+span_output=$(sentry_api_request "$SPAN_PATH" -X GET)
+echo $span_output | jq '.data[]' -e
+timeout 60 bash -c 'until $(sentry_api_request "$SPAN_PATH" -X GET | jq '.data[]' -e); do printf '.'; sleep 0.5; done'
 echo " got it!"
 echo "${_endgroup}"
 
