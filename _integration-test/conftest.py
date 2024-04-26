a4e9a0fd1c3ba7a9cf201e8d09109d955621904a
hubertdeng123 <hubertdeng123@gmail.com>
2024-04-26 11:03:55 -0700

    fix tests on master

--- _integration-test/conftest.py
+++ _integration-test/conftest.py
@@ -52,8 +52,7 @@ apt-get install -y gcc libsasl2-dev python-dev-is-python3 libldap2-dev libssl-de
         with open("sentry/requirements.txt", "w") as req_file:
             req_file.write("python-ldap\n")
         os.environ["MINIMIZE_DOWNTIME"] = "1"
-        output = subprocess.run(["./install.sh"], check=True, capture_output=True)
-        print(output)
+        subprocess.run(["./install.sh"], check=True, capture_output=True)
     # Create test user
     subprocess.run(
         [
