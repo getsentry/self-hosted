e758d23f3c0a7c61fa2f10a39d497b3d3d896516
hubertdeng123 <hubertdeng123@gmail.com>
2024-04-29 10:38:55 -0700

    give warning message to users if appropriate permissions are not set properly for docker volumes

--- install/update-docker-volume-permissions.sh
+++ install/update-docker-volume-permissions.sh
@@ -3,9 +3,13 @@ echo "${_group}Ensuring Kafka and Zookeeper volumes have correct permissions ...
 # Only supporting platforms on linux x86 platforms and not apple silicon. I'm assuming that folks using apple silicon are doing it for dev purposes and it's difficult
 # to change permissions of docker volumes since it is run in a VM.
 if [[ "$DOCKER_PLATFORM" = "linux/amd64" && -n "$(docker volume ls -q -f name=sentry-zookeeper)" && -n "$(docker volume ls -q -f name=sentry-kafka)" ]]; then
-  chmod -R a+w /var/lib/docker/volumes/sentry-zookeeper/_data || true
-  chmod -R a+w /var/lib/docker/volumes/sentry-kafka/_data || true
-  chmod -R a+w /var/lib/docker/volumes/sentry-self-hosted_sentry-zookeeper-log/_data || true
+  zookeeper_data_dir="/var/lib/docker/volumes/sentry-zookeeper/_data"
+  kafka_data_dir="/var/lib/docker/volumes/sentry-kafka/_data"
+  zookeeper_log_data_dir="/var/lib/docker/volumes/sentry-self-hosted_sentry-zookeeper-log/_data"
+  chmod -R a+w $zookeeper_data_dir $kafka_data_dir $zookeeper_log_data_dir && returncode=$? || returncode=$?
+  if [[ $returncode == "1" ]]; then
+    echo "WARNING: Error when setting appropriate permissions for zookeeper, kafka, and zookeeper log docker volumes. This may corrupt your self-hosted install."
+  fi
 fi
 
 echo "${_endgroup}"
