ffc1e1ecd1fad6c4b68b49b88933e7877810136e
hubertdeng123 <hubertdeng123@gmail.com>
2024-05-02 12:43:21 -0700

    get rid of -it option

--- install/upgrade-clickhouse.sh
+++ install/upgrade-clickhouse.sh
@@ -4,7 +4,7 @@ echo "${_group}Upgrading Clickhouse ..."
 if [[ -n "$(docker volume ls -q --filter name=sentry-clickhouse)" ]]; then
   # In order to get to 23.8, we need to first upgrade go from 21.8 -> 22.8 -> 23.3 -> 23.8
   $dc up -d clickhouse
-  version=$($dc exec -it clickhouse clickhouse-client -q 'SELECT version()')
+  version=$($dc exec clickhouse clickhouse-client -q 'SELECT version()')
   if [[ "$version" == "22.8.15.25.altinitystable" || "$version" == "21.8.12.29.altinitydev.arm" ]]; then
     $dc down clickhouse
     $dcb --build-arg BASE_IMAGE=altinity/clickhouse-server:22.8.15.25.altinitystable clickhouse
