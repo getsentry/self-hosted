369873c5af3a8e4ce9a5852e494fb05980c87281
hubertdeng123 <hubertdeng123@gmail.com>
2024-05-03 09:43:32 -0700

    add message about skipping clickhouse upgrades

--- install/upgrade-clickhouse.sh
+++ install/upgrade-clickhouse.sh
@@ -14,12 +14,14 @@ if [[ -n "$(docker volume ls -q --filter name=sentry-clickhouse)" ]]; then
 
   # In order to get to 23.8, we need to first upgrade go from 21.8 -> 22.8 -> 23.3 -> 23.8
   version=$($dc exec clickhouse clickhouse-client -q 'SELECT version()')
-  if [[ "$version" == "22.8.15.25.altinitystable" || "$version" == "21.8.12.29.altinitydev.arm" ]]; then
+  if [[ "$version" == "21.8.13.1.altinitystable" || "$version" == "21.8.12.29.altinitydev.arm" ]]; then
     $dc down clickhouse
     $dcb --build-arg BASE_IMAGE=altinity/clickhouse-server:22.8.15.25.altinitystable clickhouse
     $dc up -d clickhouse
     $dc down clickhouse
     $dcb --build-arg BASE_IMAGE=altinity/clickhouse-server:23.3.19.33.altinitystable clickhouse
+  else
+    echo "Detected clickhouse version $version. Skipping upgrades!"
   fi
   $dc down clickhouse
 fi
