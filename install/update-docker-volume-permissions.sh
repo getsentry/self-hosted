9de4b70ece8b28d83d4b2e4a4b642ff14af52ae0
Hubert Deng <hubert.deng@sentry.io>
2024-05-13 09:50:28 -0700

    fix: Make docker volume script respect compose project name (#3039)
    make the script a bit more robust in finding compose peojct name

--- install/update-docker-volume-permissions.sh
+++ install/update-docker-volume-permissions.sh
@@ -5,7 +5,7 @@ echo "${_group}Ensuring Kafka and Zookeeper volumes have correct permissions ...
 if [[ "$DOCKER_PLATFORM" = "linux/amd64" && -n "$(docker volume ls -q -f name=sentry-zookeeper)" && -n "$(docker volume ls -q -f name=sentry-kafka)" ]]; then
   zookeeper_data_dir="/var/lib/docker/volumes/sentry-zookeeper/_data"
   kafka_data_dir="/var/lib/docker/volumes/sentry-kafka/_data"
-  zookeeper_log_data_dir="/var/lib/docker/volumes/sentry-self-hosted_sentry-zookeeper-log/_data"
+  zookeeper_log_data_dir="/var/lib/docker/volumes/${COMPOSE_PROJECT_NAME}_sentry-zookeeper-log/_data"
   chmod -R a+w $zookeeper_data_dir $kafka_data_dir $zookeeper_log_data_dir && returncode=$? || returncode=$?
   if [[ $returncode == "1" ]]; then
     echo "WARNING: Error when setting appropriate permissions for zookeeper, kafka, and zookeeper log docker volumes. This may corrupt your self-hosted install. See https://github.com/confluentinc/kafka-images/issues/127 for context on why this was added."
