5301d896d79f0847a431bd739d72b15830fea42c
Jann Kleen <jann.kleen@gmail.com>
2024-05-27 12:52:39 +0200

    Update minimum docker compose requirement
    docker compose down <service> is now required.

--- install/_min-requirements.sh
+++ install/_min-requirements.sh
@@ -1,6 +1,6 @@
 # Don't forget to update the README and othes docs when you change these!
 MIN_DOCKER_VERSION='19.03.6'
-MIN_COMPOSE_VERSION='2.0.1'
+MIN_COMPOSE_VERSION='2.19.0'
 MIN_RAM_HARD=3800 # MB
 MIN_RAM_SOFT=7800 # MB
 MIN_CPU_HARD=2
