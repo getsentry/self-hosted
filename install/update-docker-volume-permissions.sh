c6389e8327acae8da477bee7b4ff9b1cd1c1b9d8
hubertdeng123 <hubertdeng123@gmail.com>
2024-05-28 11:17:11 -0700

    use correct zookeeper-log volume name

--- install/update-docker-volume-permissions.sh
+++ install/update-docker-volume-permissions.sh
@@ -3,7 +3,7 @@ echo "${_group}Ensuring Kafka and Zookeeper volumes have correct permissions ...
 # Only supporting platforms on linux x86 platforms and not apple silicon. I'm assuming that folks using apple silicon are doing it for dev purposes and it's difficult
 # to change permissions of docker volumes since it is run in a VM.
 if [[ "$DOCKER_PLATFORM" = "linux/amd64" && -n "$(docker volume ls -q -f name=sentry-zookeeper)" && -n "$(docker volume ls -q -f name=sentry-kafka)" ]]; then
-  docker run --rm -v sentry-zookeeper:/sentry-zookeeper-data -v sentry-kafka:/sentry-kafka-data -v sentry-zookeeper-log:/sentry-zookeeper-log-data busybox chmod -R a+w /sentry-zookeeper-data /sentry-kafka-data /sentry-zookeeper-log-data
+  docker run --rm -v "sentry-zookeeper:/sentry-zookeeper-data" -v "sentry-kafka:/sentry-kafka-data" -v "sentry-${COMPOSE_PROJECT_NAME}-zookeeper-log:/sentry-zookeeper-log-data" busybox chmod -R a+w /sentry-zookeeper-data /sentry-kafka-data /sentry-zookeeper-log-data
 fi
 
 echo "${_endgroup}"
