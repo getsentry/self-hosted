0dabb5a4cc6dabc3e238db60c5d1afca4afcea41
Hubert Deng <hubert.deng@sentry.io>
2024-05-29 11:30:44 -0700

    Different approach to editing permissions of docker volumes (#3084)
    * different approach to editing permissions of docker volumes

--- install/update-docker-volume-permissions.sh
+++ install/update-docker-volume-permissions.sh
@@ -2,15 +2,8 @@ echo "${_group}Ensuring Kafka and Zookeeper volumes have correct permissions ...
 
 # Only supporting platforms on linux x86 platforms and not apple silicon. I'm assuming that folks using apple silicon are doing it for dev purposes and it's difficult
 # to change permissions of docker volumes since it is run in a VM.
-if [[ "$DOCKER_PLATFORM" = "linux/amd64" && -n "$(docker volume ls -q -f name=sentry-zookeeper)" && -n "$(docker volume ls -q -f name=sentry-kafka)" ]]; then
-  docker_root_dir=$(docker info --format '{{.DockerRootDir}}')
-  zookeeper_data_dir="${docker_root_dir}/volumes/sentry-zookeeper/_data"
-  kafka_data_dir="${docker_root_dir}/volumes/sentry-kafka/_data"
-  zookeeper_log_data_dir="${docker_root_dir}/volumes/${COMPOSE_PROJECT_NAME}_sentry-zookeeper-log/_data"
-  chmod -R a+w $zookeeper_data_dir $kafka_data_dir $zookeeper_log_data_dir && returncode=$? || returncode=$?
-  if [[ $returncode == "1" ]]; then
-    echo "WARNING: Error when setting appropriate permissions for zookeeper, kafka, and zookeeper log docker volumes. This may corrupt your self-hosted install. See https://github.com/confluentinc/kafka-images/issues/127 for context on why this was added."
-  fi
+if [[ -n "$(docker volume ls -q -f name=sentry-zookeeper)" && -n "$(docker volume ls -q -f name=sentry-kafka)" ]]; then
+  docker run --rm -v "sentry-zookeeper:/sentry-zookeeper-data" -v "sentry-kafka:/sentry-kafka-data" -v "${COMPOSE_PROJECT_NAME}_sentry-zookeeper-log:/sentry-zookeeper-log-data" busybox chmod -R a+w /sentry-zookeeper-data /sentry-kafka-data /sentry-zookeeper-log-data
 fi
 
 echo "${_endgroup}"
