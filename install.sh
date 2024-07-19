a05d0f4aca0348772293f621a3a74dcbff63fb12
Joshua Li <joshuarli98@gmail.com>
2024-07-18 20:54:42 -0700

    fix: clickhouse up requires kafka and zookeeper volumes

--- install.sh
+++ install.sh
@@ -20,12 +20,12 @@ source install/check-latest-commit.sh
 source install/check-minimum-requirements.sh
 
 # Let's go! Start impacting things.
-# Upgrading clickhouse needs to come first before turning things off, since we need the old clickhouse image
+source install/create-docker-volumes.sh
+source install/update-docker-volume-permissions.sh
+# Upgrading clickhouse needs to come before turning things off, since we need the old clickhouse image
 # in order to determine whether or not the clickhouse version needs to be upgraded.
 source install/upgrade-clickhouse.sh
 source install/turn-things-off.sh
-source install/update-docker-volume-permissions.sh
-source install/create-docker-volumes.sh
 source install/ensure-files-from-examples.sh
 source install/check-memcached-backend.sh
 source install/ensure-relay-credentials.sh
