485d3ffd2f6e6bc5b64afb0721ac2e75ed5ee33a
Hubert Deng <hubert.deng@sentry.io>
2024-07-22 10:23:05 -0700

    Add errors only self-hosted infrastructure (#3190)

--- sentry/sentry.conf.example.py
+++ sentry/sentry.conf.example.py
@@ -363,3 +363,6 @@ CSP_REPORT_ONLY = True
 # https://docs.djangoproject.com/en/4.2/ref/settings/#std-setting-CSRF_TRUSTED_ORIGINS
 
 # CSRF_TRUSTED_ORIGINS = ["https://example.com", "http://127.0.0.1:9000"]
+
+# If you would like to use self-hosted Sentry with only errors enabled, please set this
+SENTRY_SELF_HOSTED_ERRORS_ONLY = env("COMPOSE_PROFILES") == "feature-complete"
