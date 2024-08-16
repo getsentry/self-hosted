e9fdf531d5fce9c693d1adb66b92e7338b25ae63
hubertdeng123 <hubertdeng123@gmail.com>
2024-08-16 14:39:46 -0700

    require less ram in MB

--- install/_min-requirements.sh
+++ install/_min-requirements.sh
@@ -1,5 +1,5 @@
 # Don't forget to update the README and othes docs when you change these!
 MIN_DOCKER_VERSION='19.03.6'
 MIN_COMPOSE_VERSION='2.19.0'
-MIN_RAM_HARD=16000 # MB
+MIN_RAM_HARD=15900 # MB
 MIN_CPU_HARD=4
