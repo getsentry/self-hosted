47d1b9d46018162df946f30f3434fe9e102614d2
Reinaldy Rafli <aldy505@proton.me>
2024-10-07 21:37:59 +0700

    chore: commence the debug

--- _unit-test/js-sdk-assets-test.sh
+++ _unit-test/js-sdk-assets-test.sh
@@ -9,14 +9,16 @@ export SETUP_JS_SDK_KEEP_OLD_ASSETS=1
 
 source install/setup-js-sdk-assets.sh
 
-sdk_files=$(docker compose run --no-deps --rm -v "sentry-nginx-www:/var/www/js-sdk" nginx ls \-lah /var/www/js-sdk)
+sdk_files=$(docker compose run --no-deps --rm -v "sentry-nginx-www:/var/www/js-sdk" nginx ls -lah /var/www/js-sdk)
 sdk_tree=$(docker compose run --no-deps --rm -v "sentry-nginx-www:/var/www/js-sdk" nginx tree /var/www/js-sdk | tail -n 1)
 
 # `sdk_files` should contains 2 lines, `7.*` and `8.*`
+echo "$sdk_files"
 test "2" == "$(echo "$sdk_files" | grep '[0-9]+$' | wc -l)"
 echo "Pass"
 
 # `sdk_tree` should outputs "3 directories, 10 files"
+echo "$sdk_tree"
 test "3 directories, 10 files" == "$(echo "$sdk_tree")"
 echo "Pass"
 
