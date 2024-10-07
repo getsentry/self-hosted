61ef22e0c8374dfdf7fb99776ac7710aa5e3fb34
Reinaldy Rafli <aldy505@proton.me>
2024-10-07 20:03:29 +0700

    ref: try without the external volume

--- install/setup-js-sdk-assets.sh
+++ install/setup-js-sdk-assets.sh
@@ -8,20 +8,20 @@ if [[ "${SETUP_JS_SDK_ASSETS:-}" == "1" ]]; then
   # `SETUP_JS_SDK_KEEP_OLD_ASSETS` to any value.
   if [[ -z "${SETUP_JS_SDK_KEEP_OLD_ASSETS:-}" ]]; then
     echo "Cleaning up old JS SDK assets..."
-    docker run --rm -v "sentry-nginx-www:/js-sdk" busybox rm -rf /js-sdk/*
+    $dcr --no-deps --rm -v "sentry-nginx-www:/var/www/js-sdk" nginx rm -rf /var/www/js-sdk/*
   fi
 
   $dbuild -t sentry-self-hosted-jq-local --platform="$DOCKER_PLATFORM" jq
 
   jq="docker run --rm -i sentry-self-hosted-jq-local"
 
-  latest_js_v7=$($dc exec web cat /usr/src/sentry/src/sentry/loader/_registry.json | $jq -r '.versions | reverse | map(select(.|any(.; startswith("7.")))) | .[0]')
-  latest_js_v8=$($dc exec web cat /usr/src/sentry/src/sentry/loader/_registry.json | $jq -r '.versions | reverse | map(select(.|any(.; startswith("8.")))) | .[0]')
+  latest_js_v7=$($dcr --no-deps -T web cat /usr/src/sentry/src/sentry/loader/_registry.json | $jq -r '.versions | reverse | map(select(.|any(.; startswith("7.")))) | .[0]')
+  latest_js_v8=$($dcr --no-deps -T web cat /usr/src/sentry/src/sentry/loader/_registry.json | $jq -r '.versions | reverse | map(select(.|any(.; startswith("8.")))) | .[0]')
 
   # Download those two using wget
   for version in "${latest_js_v7}" "${latest_js_v8}"; do
     for variant in "tracing" "tracing.replay" "replay" "tracing.replay.feedback" "feedback"; do
-      docker run --rm -v "sentry-nginx-www:/js-sdk" busybox wget -q -O /js-sdk/${version}/bundle.${variant}.min.js "https://browser.sentry-cdn.com/${version}/bundle.${variant}.min.js"
+      $dcr --no-deps --rm -v "sentry-nginx-www:/js-sdk" nginx wget -q -O /var/www/js-sdk/${version}/bundle.${variant}.min.js "https://browser.sentry-cdn.com/${version}/bundle.${variant}.min.js"
     done
   done
 
