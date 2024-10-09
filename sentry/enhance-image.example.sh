2239adef05e68438147a64d479d1fc096a7a88c9
Victor <victorelec14@gmail.com>
2024-10-09 16:40:16 +0200

    chore: replace old URLs of the repo with the new docs

--- sentry/enhance-image.example.sh
+++ sentry/enhance-image.example.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-# Enhance the base $SENTRY_IMAGE with additional dependencies, plugins - see https://github.com/getsentry/self-hosted#enhance-sentry-image
+# Enhance the base $SENTRY_IMAGE with additional dependencies, plugins - see https://develop.sentry.dev/self-hosted/#enhance-sentry-image
 # For example:
 # apt-get update
 # apt-get install -y gcc libsasl2-dev libldap2-dev libssl-dev
--- sentry/entrypoint.sh
+++ sentry/entrypoint.sh
@@ -6,7 +6,7 @@ if [ "$(ls -A /usr/local/share/ca-certificates/)" ]; then
 fi
 
 if [ -e /etc/sentry/requirements.txt ]; then
-  echo "sentry/requirements.txt is deprecated, use sentry/enhance-image.sh - see https://github.com/getsentry/self-hosted#enhance-sentry-image"
+  echo "sentry/requirements.txt is deprecated, use sentry/enhance-image.sh - see https://develop.sentry.dev/self-hosted/#enhance-sentry-image"
 fi
 
 source /docker-entrypoint.sh
