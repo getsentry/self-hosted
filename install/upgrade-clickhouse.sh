9c02d78a20fa8604f876be364bd33d822416f46b
Burak Yigit Kaya <byk@sentry.io>
2024-11-01 15:00:55 +0000

    Allow intermediate steps

--- install/upgrade-clickhouse.sh
+++ install/upgrade-clickhouse.sh
@@ -9,6 +9,11 @@ function wait_for_clickhouse() {
   done
 }
 
+function get_clickhouse_version() {
+  $dc exec clickhouse clickhouse-client -q 'SELECT version()'
+}
+
+
 # First check to see if user is upgrading by checking for existing clickhouse volume
 if [[ -n "$(docker volume ls -q --filter name=sentry-clickhouse)" ]]; then
   # Start clickhouse if it is not already running
@@ -18,16 +23,24 @@ if [[ -n "$(docker volume ls -q --filter name=sentry-clickhouse)" ]]; then
   wait_for_clickhouse
 
   # In order to get to 24.3, we need to first upgrade go from 21.8 -> 22.8 -> 23.3 -> 23.8 -> 24.3
-  version=$($dc exec clickhouse clickhouse-client -q 'SELECT version()')
+  version=$(get_clickhouse_version)
   if [[ "$version" == "21.8.13.1.altinitystable" || "$version" == "21.8.12.29.altinitydev.arm" ]]; then
     $dc down clickhouse
     $dcb --build-arg BASE_IMAGE=altinity/clickhouse-server:22.8.15.25.altinitystable clickhouse
     $dc up -d clickhouse
     wait_for_clickhouse
+  fi
+
+  version=$(get_clickhouse_version)
+  if [[ "$version" == "22.8.15.25.altinitystable" || "$version" == "22.8.15.25.altinitydev.arm" ]]; then
     $dc down clickhouse
     $dcb --build-arg BASE_IMAGE=altinity/clickhouse-server:23.3.19.33.altinitystable clickhouse
     $dc up -d clickhouse
     wait_for_clickhouse
+  fi
+  
+  version=$(get_clickhouse_version)
+  if [[ "$version" == "23.3.19.33.altinitystable" || "$version" == "23.3.19.33.altinitydev.arm" ]]; then
     $dc down clickhouse
     $dcb --build-arg BASE_IMAGE=altinity/clickhouse-server:23.8.11.29.altinitystable clickhouse
     $dc up -d clickhouse
