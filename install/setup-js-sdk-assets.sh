fb01b2bfe40c7e08c0d75716d9c9c1ca583ba6dd
Reinaldy Rafli <aldy505@proton.me>
2024-11-06 10:52:28 +0700

    fix: make curl silent

--- install/setup-js-sdk-assets.sh
+++ install/setup-js-sdk-assets.sh
@@ -35,12 +35,14 @@ if [[ "${SETUP_JS_SDK_ASSETS:-}" == "1" ]]; then
     $dcr --no-deps --rm -v "sentry-nginx-www:/var/www" nginx mkdir -p /var/www/js-sdk/${version}
     for variant in "bundle" "bundle.tracing" "bundle.tracing.replay" "bundle.replay" "bundle.tracing.replay.feedback" "bundle.feedback"; do
       # Taken from https://superuser.com/questions/272265/getting-curl-to-output-http-status-code#comment1025992_272273
-      status_code=$($dcr --no-deps --rm nginx curl -I https://browser.sentry-cdn.com/${version}/${variant}.min.js 2>/dev/null | head -n 1 | cut -d$' ' -f2)
+      status_code=$($dcr --no-deps --rm nginx curl --silent -I https://browser.sentry-cdn.com/${version}/${variant}.min.js 2>/dev/null | head -n 1 | cut -d$' ' -f2)
       if [[ "$status_code" != "200" ]]; then
         echo "Skipping download of JS SDK v${version} for variant ${variant}, because the status code was ${status_code} (non 200)"
         continue
       fi
 
+      echo "Downloading JS SDK v${version} for variant ${variant}..."
+      
       $dcr --no-deps --rm -v "sentry-nginx-www:/var/www" nginx wget -q -O /var/www/js-sdk/${version}/${variant}.min.js "https://browser.sentry-cdn.com/${version}/${variant}.min.js"
     done
   done
