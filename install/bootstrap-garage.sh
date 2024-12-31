7b28dfb97a53fbbacdab7d45153b8f352af1a2f0
Burak Yigit Kaya <byk@sentry.io>
2024-12-31 16:42:03 +0300

    fix moar bash

--- install/bootstrap-garage.sh
+++ install/bootstrap-garage.sh
@@ -10,12 +10,12 @@ if [[ $($garage bucket list | tail -1 | awk '{print $1}') != 'nodestore' ]]; the
 
   $garage bucket create nodestore
   key_info=$($garage key create nodestore-key | head -3 | tail -2)
-  key_id=$(echo "$key_info" | head -1 | awk '{print $1}')
-  key_secret=$(echo "$key_info" | tail -1 | awk '{print $1}')
+  key_id=$(echo "$key_info" | head -1 | awk '{print $3}')
+  key_secret=$(echo "$key_info" | tail -1 | awk '{print $3}')
 
   $garage bucket allow --read --write --owner nodestore --key nodestore-key
 
-  sed -i -e "s/<GARAGE_KEY_ID>/$key_info/" $SENTRY_CONFIG_PY
+  sed -i -e "s/<GARAGE_KEY_ID>/$key_id/" $SENTRY_CONFIG_PY
   sed -i -e "s/<GARAGE_SECRET_KEY>/$key_secret/" $SENTRY_CONFIG_PY
   echo "Set Garage keys for SENTRY_NODESTORE_OPTIONS in $SENTRY_CONFIG_PY"
 else
