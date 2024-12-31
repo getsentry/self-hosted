5dae37a86792c3a9af3ad0d57a161194f6bac8fe
Burak Yigit Kaya <byk@sentry.io>
2024-12-31 13:39:15 +0300

    some fixes

--- install/set-up-and-migrate-database.sh
+++ install/set-up-and-migrate-database.sh
@@ -12,12 +12,12 @@ if [[ -n "${SKIP_DB_MIGRATIONS:-}" ]]; then
 
   # Using django ORM to provide broader support for users with external databases
   $dcr web shell -c "
-  from django.db import connection
+from django.db import connection
 
-  with connection.cursor() as cursor:
-    cursor.execute('ALTER TABLE IF EXISTS sentry_groupedmessage DROP CONSTRAINT IF EXISTS sentry_groupedmessage_project_id_id_515aaa7e_uniq;')
-    cursor.execute('DROP INDEX IF EXISTS sentry_groupedmessage_project_id_id_515aaa7e_uniq;')
-  "
+with connection.cursor() as cursor:
+  cursor.execute('ALTER TABLE IF EXISTS sentry_groupedmessage DROP CONSTRAINT IF EXISTS sentry_groupedmessage_project_id_id_515aaa7e_uniq;')
+  cursor.execute('DROP INDEX IF EXISTS sentry_groupedmessage_project_id_id_515aaa7e_uniq;')
+"
 
   if [[ -n "${CI:-}" || "${SKIP_USER_CREATION:-0}" == 1 ]]; then
     $dcr web upgrade --noinput --create-kafka-topics
