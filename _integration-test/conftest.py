85a8aed1d85e1ab4c8e1cb8b87ab38622fe65696
Burak Yigit Kaya <byk@sentry.io>
2025-01-09 20:30:20 +0000

    move out of band

--- _integration-test/conftest.py
+++ _integration-test/conftest.py
@@ -19,27 +19,6 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope="session", autouse=True)
 def configure_self_hosted_environment(request):
-    """Back up the state of DB volumes"""
-    try:
-        subprocess.run(
-            [
-                "rsync",
-                "-avWm",
-                "--no-compress",
-                "--mkpath",
-                "/var/lib/docker/volumes/sentry-postgres",
-                "/var/lib/docker/volumes/sentry-clickhouse",
-                "/var/lib/docker/volumes/sentry-kafka",
-                join(os.environ["RUNNER_TEMP"], "volumes", ""),
-            ],
-            check=True,
-            capture_output=True,
-        )
-    except subprocess.CalledProcessError as e:
-        print(e.stdout.decode())
-        print(e.stderr.decode())
-        raise e
-
     subprocess.run(
         ["docker", "compose", "--ansi", "never", "up", "--wait"],
         check=True,
