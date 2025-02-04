06d287e7970bf988dfeacccb795a2b6106828208
Reinaldy Rafli <aldy505@proton.me>
2025-02-04 08:15:04 +0700

    fix(tests): up one dir

--- _unit-test/merge-env-file-test.sh
+++ _unit-test/merge-env-file-test.sh
@@ -3,7 +3,7 @@
 # This is a test file for a part of `_lib.sh`, where we read `.env.custom` file if there is one.
 # We only want to give very minimal value to the `.env.custom` file, and expect that it would
 # be merged with the original `.env` file, with the `.env.custom` file taking precedence.
-cat <<EOF >.env.custom
+cat <<EOF >../.env.custom
 SENTRY_EVENT_RETENTION_DAYS=10
 EOF
 
