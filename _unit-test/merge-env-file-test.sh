fef1a3b320dca579325428f8dac9dfd35a38252e
Reinaldy Rafli <aldy505@proton.me>
2025-02-04 08:24:38 +0700

    fix(test): replace path back to current path

--- _unit-test/merge-env-file-test.sh
+++ _unit-test/merge-env-file-test.sh
@@ -3,9 +3,11 @@
 # This is a test file for a part of `_lib.sh`, where we read `.env.custom` file if there is one.
 # We only want to give very minimal value to the `.env.custom` file, and expect that it would
 # be merged with the original `.env` file, with the `.env.custom` file taking precedence.
-cat <<EOF >../.env.custom
+cat <<EOF >".env.custom"
 SENTRY_EVENT_RETENTION_DAYS=10
 EOF
+
+ls -lah # for debugging
 
 # The `_test_setup.sh` script sources `install/_lib.sh`, so.. finger crossed this should works.
 source _unit-test/_test_setup.sh
