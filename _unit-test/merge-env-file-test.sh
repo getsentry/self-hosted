69fb14db53f71da8dc811b518c919d37074eb402
Reinaldy Rafli <aldy505@proton.me>
2025-02-04 08:01:36 +0700

    fix(tests): wrong symbol for accessing variable

--- _unit-test/merge-env-file-test.sh
+++ _unit-test/merge-env-file-test.sh
@@ -10,13 +10,13 @@ EOF
 # The `_test_setup.sh` script sources `install/_lib.sh`, so.. finger crossed this should works.
 source _unit-test/_test_setup.sh
 
-echo "Expecting SENTRY_EVENT_RETENTION_DAYS to be 10, got $(SENTRY_EVENT_RETENTION_DAYS)"
+echo "Expecting SENTRY_EVENT_RETENTION_DAYS to be 10, got ${SENTRY_EVENT_RETENTION_DAYS}"
 test "$SENTRY_EVENT_RETENTION_DAYS" == "10"
 echo "Pass"
-echo "Expecting SENTRY_BIND to be 9000, got $(SENTRY_BIND)"
+echo "Expecting SENTRY_BIND to be 9000, got ${SENTRY_BIND}"
 test "$SENTRY_BIND" == "9000"
 echo "Pass"
-echo "Expecting COMPOSE_PROJECT_NAME to be sentry-self-hosted, got $(COMPOSE_PROJECT_NAME)"
+echo "Expecting COMPOSE_PROJECT_NAME to be sentry-self-hosted, got ${COMPOSE_PROJECT_NAME}" 
 test "$COMPOSE_PROJECT_NAME" == "sentry-self-hosted"
 echo "Pass"
 
