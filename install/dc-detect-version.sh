d885dd331ff1991b95d285969ef2bca5add94ce0
Amin Vakil <info@aminvakil.com>
2025-03-03 16:46:31 +0330

    Use docker-compose if version is gte docker compose (#3595)
    Fixes #3587 
    
    This PR tries to use docker-compose if its version is greater than docker compose.

--- install/dc-detect-version.sh
+++ install/dc-detect-version.sh
@@ -10,6 +10,23 @@ echo "${_group}Initializing Docker Compose ..."
 
 # To support users that are symlinking to docker-compose
 dc_base="$(docker compose version &>/dev/null && echo 'docker compose' || echo 'docker-compose')"
+dc_base_standalone="$(docker-compose version &>/dev/null && echo 'docker-compose' || echo '')"
+
+COMPOSE_VERSION=$($dc_base version --short || echo '')
+STANDALONE_COMPOSE_VERSION=$($dc_base_standalone version --short &>/dev/null || echo '')
+
+if [[ -z "$COMPOSE_VERSION" && -z "$STANDALONE_COMPOSE_VERSION" ]]; then
+  echo "FAIL: Docker Compose is required to run self-hosted"
+  exit 1
+fi
+
+if [[ ! -z "${STANDALONE_COMPOSE_VERSION}" ]]; then
+  if [[ "$(vergte ${COMPOSE_VERSION//v/} ${STANDALONE_COMPOSE_VERSION//v/})" -eq 1 ]]; then
+    COMPOSE_VERSION="${STANDALONE_COMPOSE_VERSION}"
+    dc_base="$dc_base_standalone"
+  fi
+fi
+
 if [[ "$(basename $0)" = "install.sh" ]]; then
   dc="$dc_base --ansi never --env-file ${_ENV}"
 else
