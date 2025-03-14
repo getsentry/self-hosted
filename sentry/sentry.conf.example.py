30f8d0125f919e2cfef2b47edf80a9b0611b74c6
Reinaldy Rafli <aldy505@proton.me>
2025-03-14 13:46:30 +0000

    feat(features): enable trace view (#3617)

--- sentry/sentry.conf.example.py
+++ sentry/sentry.conf.example.py
@@ -298,6 +298,7 @@ SENTRY_FEATURES.update(
             "organizations:dashboards-rh-widget",
             "organizations:metrics-extraction",
             "organizations:transaction-metrics-extraction",
+            "organizations:trace-view-v1",
             "projects:custom-inbound-filters",
             "projects:data-forwarding",
             "projects:discard-groups",
