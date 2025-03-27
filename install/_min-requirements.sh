ae05091f9ff55c664ef453d121fe5c89c48a4b9d
Kliachin Aleksei <madest92@gmail.com>
2025-03-27 12:14:02 +0300

    Minimum requirements for 'errors-only' profile (#3634)
    Using the [errors-only](https://develop.sentry.dev/self-hosted/experimental/errors-only/) profile, fewer resources are required. About 2 times.

--- install/_min-requirements.sh
+++ install/_min-requirements.sh
@@ -4,6 +4,10 @@ MIN_COMPOSE_VERSION='2.32.2'
 
 # 16 GB minimum host RAM, but there'll be some overhead outside of what
 # can be allotted to docker
-MIN_RAM_HARD=14000 # MB
-
-MIN_CPU_HARD=4
+if [[ "$COMPOSE_PROFILES" == "errors-only" ]]; then
+  MIN_RAM_HARD=7000 # MB
+  MIN_CPU_HARD=2
+else
+  MIN_RAM_HARD=14000 # MB
+  MIN_CPU_HARD=4
+fi
