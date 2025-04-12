53412bbefbc09a076a0beb52c10efa0589a6b52e
Amin Vakil <info@aminvakil.com>
2025-04-12 18:59:38 +0330

    Fix STANDALONE_COMPOSE_VERSION variable setting (#3654)

--- install/dc-detect-version.sh
+++ install/dc-detect-version.sh
@@ -13,7 +13,7 @@ dc_base="$(docker compose version --short &>/dev/null && echo 'docker compose' |
 dc_base_standalone="$(docker-compose version --short &>/dev/null && echo 'docker-compose' || echo '')"
 
 COMPOSE_VERSION=$([ -n "$dc_base" ] && $dc_base version --short || echo '')
-STANDALONE_COMPOSE_VERSION=$([ -n "$dc_base_standalone" ] && $dc_base_standalone version --short &>/dev/null || echo '')
+STANDALONE_COMPOSE_VERSION=$([ -n "$dc_base_standalone" ] && $dc_base_standalone version --short || echo '')
 
 if [[ -z "$COMPOSE_VERSION" && -z "$STANDALONE_COMPOSE_VERSION" ]]; then
   echo "FAIL: Docker Compose is required to run self-hosted"
