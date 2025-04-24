fc3d976ebeda99281a444361cbe32160b9384cca
Daniel Bunte <daniel.bunte@statnett.no>
2025-04-24 16:46:52 +0200

    fix(install): Substitute --force-rm for podman

--- install/build-docker-images.sh
+++ install/build-docker-images.sh
@@ -3,10 +3,14 @@ echo "${_group}Building and tagging Docker images ..."
 echo ""
 # Build any service that provides the image sentry-self-hosted-local first,
 # as it is used as the base image for sentry-cleanup-self-hosted-local.
-$dcb --force-rm web
+dcb_force="$dcb --force-rm"
+if [[ "$CONTAINER_ENGINE" == "docker" ]]; then
+  dcb_force="$dcb --podman-rm-args='--force'"
+fi
+$dcb_force web
 # Build each other service individually to localize potential failures better.
 for service in $($dc config --services); do
-  $dcb --force-rm "$service"
+  $dcb_force "$service"
 done
 echo ""
 echo "Docker images built."
