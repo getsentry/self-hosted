b679f063e4e420dfbee161d4386905cbab18e4a8
Daniel Bunte <daniel.bunte@statnett.no>
2025-04-28 18:18:42 +0200

    chore(install): Switch docker/podman if-else in install/update-docker-images.sh

--- install/update-docker-images.sh
+++ install/update-docker-images.sh
@@ -1,6 +1,10 @@
 echo "${_group}Fetching and updating $CONTAINER_ENGINE images ..."
 
-if [ "$CONTAINER_ENGINE" = "docker" ]; then
+if [ "$CONTAINER_ENGINE" = "podman" ]; then
+    # podman compose doesn't have the --ignore-pull-failures option, so can just
+    # run the command normally
+    $dc pull || true
+else
     # We tag locally built images with a '-self-hosted-local' suffix. `docker
     # compose pull` tries to pull these too and shows a 404 error on the console
     # which is confusing and unnecessary. To overcome this, we add the
@@ -8,10 +12,6 @@ if [ "$CONTAINER_ENGINE" = "docker" ]; then
     # having this '-onpremise-local' suffix.
 
     $dc pull --ignore-pull-failures 2>&1 | grep -v -- -self-hosted-local || true
-else
-  # podman compose doesn't have the --ignore-pull-failures option, so can just
-  # run the command normally
-  $dc pull || true
 fi
 
 # We may not have the set image on the repo (local images) so allow fails
