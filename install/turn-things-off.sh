b8a02069d5162fdb99f92a777f253c596fe5a5b1
Daniel Bunte <daniel.bunte.84@gmail.com>
2025-04-28 18:13:25 +0200

    Update install/turn-things-off.sh
    Co-authored-by: Burak Yigit Kaya <ben@byk.im>

--- install/turn-things-off.sh
+++ install/turn-things-off.sh
@@ -5,15 +5,15 @@ if [[ -n "$MINIMIZE_DOWNTIME" ]]; then
   $dc rm -fsv $($dc config --services | grep -v -E '^(nginx|relay)$')
 else
   # Clean up old stuff and ensure nothing is working while we install/update
-  if [ "$CONTAINER_ENGINE" = "docker" ]; then
-    $dc down -t $STOP_TIMEOUT --rmi local --remove-orphans
-  elif [ "$CONTAINER_ENGINE" = "podman" ]; then
+  if [ "$CONTAINER_ENGINE" = "podman" ]; then
     $dc down -t $STOP_TIMEOUT --remove-orphans
     dangling_images=$($CONTAINER_ENGINE images --quiet --filter dangling=true)
     if [ -n "$dangling_images" ]; then
       # Remove dangling images
       $CONTAINER_ENGINE rmi -f $dangling_images
     fi
+  else
+    $dc down -t $STOP_TIMEOUT --rmi local --remove-orphans      
   fi
 fi
 
