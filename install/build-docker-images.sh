ca4055731a8612b3c266930c646ee7aa2252d3be
Burak Yigit Kaya <byk@sentry.io>
2025-05-07 21:15:19 +0100

    well, it should be --podman-build

--- install/build-docker-images.sh
+++ install/build-docker-images.sh
@@ -5,7 +5,7 @@ echo ""
 # as it is used as the base image for sentry-cleanup-self-hosted-local.
 dcb_force="$dcb --force-rm"
 if [[ "$CONTAINER_ENGINE" == "podman" ]]; then
-  dcb_force="$dcb --podman-rm-args=--force"
+  dcb_force="$dcb --podman-build-args=--force"
 fi
 $dcb_force web
 # Build each other service individually to localize potential failures better.
