a2792f714f0c985958f906b753ecc2ce1f353c85
Burak Yigit Kaya <byk@sentry.io>
2025-05-07 22:18:21 +0100

    --force-rm is no longer anyway

--- install/build-docker-images.sh
+++ install/build-docker-images.sh
@@ -3,14 +3,10 @@ echo "${_group}Building and tagging Docker images ..."
 echo ""
 # Build any service that provides the image sentry-self-hosted-local first,
 # as it is used as the base image for sentry-cleanup-self-hosted-local.
-dcb_force="$dcb --force-rm"
-if [[ "$CONTAINER_ENGINE" == "podman" ]]; then
-  dcb_force="$dcb"
-fi
-$dcb_force web
+$dcb web
 # Build each other service individually to localize potential failures better.
 for service in $($dc config --services); do
-  $dcb_force "$service"
+  $dcb "$service"
 done
 echo ""
 echo "Docker images built."
