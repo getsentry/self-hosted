c8ee02de19c71dd89f722060f6a7d90d0c619378
Mark Story <mark@mark-story.com>
2025-06-11 10:57:24 -0400

    feat: Add taskbroker + worker + scheduler (#3738)

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -4,7 +4,7 @@ set -eu
 OLD_VERSION="$1"
 NEW_VERSION="$2"
 
-sed -i -e "s/^\(SENTRY\|SNUBA\|RELAY\|SYMBOLICATOR\|VROOM\)_IMAGE=\([^:]\+\):.\+\$/\1_IMAGE=\2:$NEW_VERSION/" .env
+sed -i -e "s/^\(SENTRY\|SNUBA\|RELAY\|SYMBOLICATOR\|TASKBROKER\|VROOM\)_IMAGE=\([^:]\+\):.\+\$/\1_IMAGE=\2:$NEW_VERSION/" .env
 sed -i -e "s/^\# Self-Hosted Sentry .*/# Self-Hosted Sentry $NEW_VERSION/" README.md
 
 echo "New version: $NEW_VERSION"
