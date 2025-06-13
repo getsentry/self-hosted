66c057b4e2281ec009811023fad33ebcc9131be0
Nikita Korolev <66738864+doc-sheet@users.noreply.github.com>
2025-06-14 02:50:26 +0300

    enable shell linter for more scripts (#3748)

--- scripts/bump-version.sh
+++ scripts/bump-version.sh
@@ -7,4 +7,5 @@ NEW_VERSION="$2"
 sed -i -e "s/^\(SENTRY\|SNUBA\|RELAY\|SYMBOLICATOR\|TASKBROKER\|VROOM\)_IMAGE=\([^:]\+\):.\+\$/\1_IMAGE=\2:$NEW_VERSION/" .env
 sed -i -e "s/^\# Self-Hosted Sentry .*/# Self-Hosted Sentry $NEW_VERSION/" README.md
 
+[ -z "$OLD_VERSION" ] || echo "Previous version: $OLD_VERSION"
 echo "New version: $NEW_VERSION"
