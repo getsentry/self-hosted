5ad8f80e0e3b0494fdeb7a22b1eb5b1ebaf02401
hubertdeng123 <hubertdeng123@gmail.com>
2025-06-17 11:02:50 -0700

    output permissions

--- install/ensure-correct-permissions-profiles-dir.sh
+++ install/ensure-correct-permissions-profiles-dir.sh
@@ -2,4 +2,5 @@
 
 echo "${_group}Ensuring correct permissions on profiles directory ..."
 docker run --user root --rm -v sentry-vroom:/var/lib/sentry-profiles alpine chown -R 1000:1000 /var/lib/sentry-profiles
+docker run --user root --rm -v sentry-vroom:/var/lib/sentry-profiles alpine ls -l /var/lib/
 echo "${_endgroup}"
