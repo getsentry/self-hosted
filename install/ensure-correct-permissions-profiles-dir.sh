5419a65503dadc137e38bd9e87abf377daac3190
Burak Yigit Kaya <byk@sentry.io>
2025-06-17 21:04:36 +0100

    undo silly perms and debug

--- install/ensure-correct-permissions-profiles-dir.sh
+++ install/ensure-correct-permissions-profiles-dir.sh
@@ -3,5 +3,5 @@
 # TODO: Remove this after the next hard-stop
 
 echo "${_group}Ensuring correct permissions on profiles directory ..."
-$dcr --no-deps --entrypoint /bin/bash --user root vroom -c 'chown -R vroom:vroom /var/vroom/sentry-profiles && chmod o+x /var/lib/ && chmod -R o+rwx /var/vroom/sentry-profiles && ls -la /var/vroom/sentry-profiles && ls -la /var/lib'
+$dcr --no-deps --entrypoint /bin/bash --user root vroom -c 'chown -R vroom:vroom /var/vroom/sentry-profiles && chmod -R o+rwx /var/vroom/sentry-profiles'
 echo "${_endgroup}"
