65459a8ed1f5dc6ee93adf8725caff8cca9dc961
hubertdeng123 <hubertdeng123@gmail.com>
2025-06-17 13:17:13 -0700

    try this

--- install/ensure-correct-permissions-profiles-dir.sh
+++ install/ensure-correct-permissions-profiles-dir.sh
@@ -3,5 +3,5 @@
 # TODO: Remove this after the next hard-stop
 
 echo "${_group}Ensuring correct permissions on profiles directory ..."
-$dcr --no-deps --entrypoint /bin/bash vroom -c 'chown -R 1000:1000 /var/lib/sentry-profiles'
+$dcr --rm --no-deps --entrypoint /bin/bash vroom -c 'chown -R 1000:1000 /var/lib/sentry-profiles'
 echo "${_endgroup}"
