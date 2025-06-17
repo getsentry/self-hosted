c8f9481f47adbacacb27894e6f67d6d61dd56f19
Burak Yigit Kaya <byk@sentry.io>
2025-06-17 19:28:13 +0100

    also no deps

--- install/ensure-correct-permissions-profiles-dir.sh
+++ install/ensure-correct-permissions-profiles-dir.sh
@@ -3,5 +3,5 @@
 # TODO: Remove this after the next hard-stop
 
 echo "${_group}Ensuring correct permissions on profiles directory ..."
-$dcr --entrypoint /bin/bash --user root vroom -c 'chown -R vroom:vroom /var/lib/sentry-profiles'
+$dcr --no-deps --entrypoint /bin/bash --user root vroom -c 'chown -R vroom:vroom /var/lib/sentry-profiles'
 echo "${_endgroup}"
