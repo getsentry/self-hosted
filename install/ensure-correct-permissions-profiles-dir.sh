0c630bed3d46d7653673472fbce854dff924fa20
hubertdeng123 <hubertdeng123@gmail.com>
2025-06-17 11:20:25 -0700

    also try with user root

--- install/ensure-correct-permissions-profiles-dir.sh
+++ install/ensure-correct-permissions-profiles-dir.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "${_group}Ensuring correct permissions on profiles directory ..."
-$dc run -d --rm vroom chown -R 1000:1000 /var/lib/sentry-profiles
+$dc run -d --rm --user root vroom chown -R 1000:1000 /var/lib/sentry-profiles
 echo "${_endgroup}"
