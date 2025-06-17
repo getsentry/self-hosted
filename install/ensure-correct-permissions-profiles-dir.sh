8ced923098aff0b540f485cc1900a4212bbc4c0f
hubertdeng123 <hubertdeng123@gmail.com>
2025-06-17 11:20:42 -0700

    and entrypoint bash

--- install/ensure-correct-permissions-profiles-dir.sh
+++ install/ensure-correct-permissions-profiles-dir.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 echo "${_group}Ensuring correct permissions on profiles directory ..."
-$dc run -d --rm --user root vroom chown -R 1000:1000 /var/lib/sentry-profiles
+$dc run -d --rm --entrypoint /bin/bash --user root vroom chown -R 1000:1000 /var/lib/sentry-profiles
 echo "${_endgroup}"
