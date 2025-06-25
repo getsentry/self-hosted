33d759609d8f7d7707ae4b751b49ed67a95a5d35
Reinaldy Rafli <github@reinaldyrafli.com>
2025-06-25 21:32:09 +0700

    specify profile arg on dcr declaration

--- install/ensure-correct-permissions-profiles-dir.sh
+++ install/ensure-correct-permissions-profiles-dir.sh
@@ -4,6 +4,6 @@
 
 echo "${_group}Ensuring correct permissions on profiles directory ..."
 
-$dcr --profile feature-complete --no-deps --entrypoint /bin/bash --user root vroom -c 'chown -R vroom:vroom /var/vroom/sentry-profiles && chmod -R o+rwx /var/vroom/sentry-profiles'
+$dcr --no-deps --entrypoint /bin/bash --user root vroom -c 'chown -R vroom:vroom /var/vroom/sentry-profiles && chmod -R o+rwx /var/vroom/sentry-profiles'
 
 echo "${_endgroup}"
