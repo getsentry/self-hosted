2bb3fbf682fe029fd1bd39c2de5f9cd0f79ba01a
Reinaldy Rafli <github@reinaldyrafli.com>
2025-07-25 10:45:24 +0700

    fix(scripts): every known flags should be shifted before executing the sentry <foo> command
    Fixes https://github.com/getsentry/self-hosted/issues/3526
    
    Previously, if this was executed:
    ```bash
    ./scripts/backup.sh --no-report-self-hosted-issues
    ```
    
    The final backup command that will be executed is:
    ```bash
    docker compose run -v "${PWD}/sentry:/sentry-data/backup" --rm -T -e SENTRY_LOG_LEVEL=CRITICAL web export --no-report-self-hosted-issues /sentry-data/backup/backup.json
    ```
    
    Which is invalid. This PR strips all known flags specific to self-hosted before passing it onto Sentry.
    
    One other option is to enable "ignore unknown options" on click (the CLI dependency) on Sentry, but I don't want to go to that route.

--- scripts/reset.sh
+++ scripts/reset.sh
@@ -1,4 +1,17 @@
 #!/usr/bin/env bash
+MINIMIZE_DOWNTIME="${MINIMIZE_DOWNTIME:-}"
+REPORT_SELF_HOSTED_ISSUES="${REPORT_SELF_HOSTED_ISSUES:-}"
+
+while (($#)); do
+  case "$1" in
+  --report-self-hosted-issues) REPORT_SELF_HOSTED_ISSUES=1 ;;
+  --no-report-self-hosted-issues) REPORT_SELF_HOSTED_ISSUES=0 ;;
+  --minimize-downtime) MINIMIZE_DOWNTIME=1 ;;
+  *) version=$1 ;;
+  esac
+  shift
+done
+
 cmd=reset
 source scripts/_lib.sh
 $cmd
