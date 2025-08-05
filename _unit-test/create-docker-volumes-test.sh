3a8c05036b196d4b3638feef49c1a07629918a82
Reinaldy Rafli <github@reinaldyrafli.com>
2025-08-05 15:25:42 +0700

    debug: aaaaaaaaaaaaaaaaaaaaaaarrrrggggggghhhhhhhhhhhhhhh

--- _unit-test/create-docker-volumes-test.sh
+++ _unit-test/create-docker-volumes-test.sh
@@ -24,6 +24,7 @@ test "$before" == "" || test "$before" == "$expected_volumes"
 source install/create-docker-volumes.sh
 
 after=$(get_volumes)
+echo "$after"
 test "$after" == "$expected_volumes"
 
 report_success
