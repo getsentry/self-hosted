9bdbf03970126b23df7f92843161cc17fa16c133
Iven Schlenther <stossenhd@gmail.com>
2025-08-19 09:19:54 +0200

    fix(enhancement): search for permissions on docker container instead of host

--- install/ensure-correct-permissions-profiles-dir.sh
+++ install/ensure-correct-permissions-profiles-dir.sh
@@ -5,13 +5,10 @@
 echo "${_group}Ensuring correct permissions on profiles directory ..."
 
 # Check if the parent directory of /var/vroom/sentry-profiles is already owned by vroom:vroom
-$dcr --no-deps --entrypoint /bin/bash --user root vroom -c '
-  if [ "$(stat -c "%U:%G" /var/vroom/sentry-profiles 2>/dev/null)" = "vroom:vroom" ]; then
-    echo "Ownership already correct. Skipping chown."
-  else
-    chown -R vroom:vroom /var/vroom/sentry-profiles \
-    && chmod -R o+rwx /var/vroom/sentry-profiles
-  fi
-'
+if [ "$($dcr --no-deps --entrypoint /bin/bash --user root vroom -c "stat -c '%U:%G' /var/vroom/sentry-profiles" 2>/dev/null)" = "vroom:vroom" ]; then
+  echo "Ownership of /var/vroom/sentry-profiles is already set to vroom:vroom. Skipping chown."
+else
+  $dcr --no-deps --entrypoint /bin/bash --user root vroom -c 'chown -R vroom:vroom /var/vroom/sentry-profiles && chmod -R o+rwx /var/vroom/sentry-profiles'
+fi
 
 echo "${_endgroup}"
