b69d1137202a345edd953a1f4eced8be41a44e63
Iven Schlenther <stossenhd@gmail.com>
2025-08-19 08:55:45 +0200

    fix(enhancement): search for permissions on docker container instead of host and combine it in one command for performance enhancement
    Resolves #3882

--- install/ensure-correct-permissions-profiles-dir.sh
+++ install/ensure-correct-permissions-profiles-dir.sh
@@ -5,10 +5,13 @@
 echo "${_group}Ensuring correct permissions on profiles directory ..."
 
 # Check if the parent directory of /var/vroom/sentry-profiles is already owned by vroom:vroom
-if [ "$(stat -c '%U:%G' /var/vroom)" = "vroom:vroom" ]; then
-  echo "Ownership of /var/vroom is already set to vroom:vroom. Skipping chown."
-else
-  $dcr --no-deps --entrypoint /bin/bash --user root vroom -c 'chown -R vroom:vroom /var/vroom/sentry-profiles && chmod -R o+rwx /var/vroom/sentry-profiles'
-fi
+$dcr --no-deps --entrypoint /bin/bash --user root vroom -c '
+  if [ "$(stat -c "%U:%G" /var/vroom/sentry-profiles 2>/dev/null)" = "vroom:vroom" ]; then
+    echo "Ownership already correct. Skipping chown."
+  else
+    chown -R vroom:vroom /var/vroom/sentry-profiles \
+    && chmod -R o+rwx /var/vroom/sentry-profiles
+  fi
+'
 
 echo "${_endgroup}"
