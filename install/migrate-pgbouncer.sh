b2cb6f400f3b4b21629b6ca0362da4d02e659945
Reinaldy Rafli <github@reinaldyrafli.com>
2025-08-23 19:16:50 +0700

    feat: try to use standard grep and sed

--- install/migrate-pgbouncer.sh
+++ install/migrate-pgbouncer.sh
@@ -28,13 +28,11 @@ if [ "$SETUP_PGBOUNCER_MIGRATION" == "1" ]; then
   # }
   # ```
 
-  # Found this from https://stackoverflow.com/a/2686705/3153224
-  if pcregrep -M '^DATABASES[[:space:]]*=[[:space:]]*{\n[[:space:]]*"default"[[:space:]]*:[[:space:]]*{\n[[:space:]]*"ENGINE"[[:space:]]*:[[:space:]]*"sentry.db.postgres",\n[[:space:]]*"NAME"[[:space:]]*:[[:space:]]*"postgres",\n[[:space:]]*"USER"[[:space:]]*:[[:space:]]*"postgres",\n[[:space:]]*"PASSWORD"[[:space:]]*:[[:space:]]*"",\n[[:space:]]*"HOST"[[:space:]]*:[[:space:]]*"postgres",\n[[:space:]]*"PORT"[[:space:]]*:[[:space:]]*"",\n[[:space:]]*}\n[[:space:]]*}' "$SENTRY_CONFIG_PY"; then
+  if sed -n '/^DATABASES = {$/,/^}$/p' "$SENTRY_CONFIG_PY" | grep -q '"HOST": "postgres"'; then
     echo "Migrating $SENTRY_CONFIG_PY to use PGBouncer"
     sed -i 's/"HOST": "postgres"/"HOST": "pgbouncer"/' "$SENTRY_CONFIG_PY"
     echo "Migrated $SENTRY_CONFIG_PY to use PGBouncer"
-  # See if we already have pgbouncer set. If it is, then we just say "ok you're good".
-  elif pcregrep -M '^DATABASES[[:space:]]*=[[:space:]]*{\n[[:space:]]*"default"[[:space:]]*:[[:space:]]*{\n[[:space:]]*"ENGINE"[[:space:]]*:[[:space:]]*"sentry.db.postgres",\n[[:space:]]*"NAME"[[:space:]]*:[[:space:]]*"postgres",\n[[:space:]]*"USER"[[:space:]]*:[[:space:]]*"postgres",\n[[:space:]]*"PASSWORD"[[:space:]]*:[[:space:]]*"",\n[[:space:]]*"HOST"[[:space:]]*:[[:space:]]*"pgbouncer",\n[[:space:]]*"PORT"[[:space:]]*:[[:space:]]*"",\n[[:space:]]*}\n[[:space:]]*}' "$SENTRY_CONFIG_PY"; then
+  elif sed -n '/^DATABASES = {$/,/^}$/p' "$SENTRY_CONFIG_PY" | grep -q '"HOST": "pgbouncer"'; then
     echo "Found pgbouncer in $SENTRY_CONFIG_PY, I'm assuming you're good! :)"
   else
     echo "⚠️ You don't have standard configuration for Postgres in $SENTRY_CONFIG_PY, skipping pgbouncer migration. I'm assuming you know what you're doing."
