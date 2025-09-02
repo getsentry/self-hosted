d4ef9943d23d2ae4ad41520b3647f6adf705f14b
Reinaldy Rafli <github@reinaldyrafli.com>
2025-09-02 18:53:18 +0700

    feat: don't exit on install script

--- install/ensure-correct-permissions-profiles-dir.sh
+++ install/ensure-correct-permissions-profiles-dir.sh
@@ -3,18 +3,15 @@
 # TODO: Remove this after the next hard-stop
 
 # Should only run when `$COMPOSE_PROFILES` is set to `feature-complete`
-if [[ "$COMPOSE_PROFILES" == "errors-only" ]]; then
-  echo "Skipping permissions check for profiles directory."
-  exit 0
+if [[ "$COMPOSE_PROFILES" == "feature-complete" ]]; then
+  echo "${_group}Ensuring correct permissions on profiles directory ..."
+
+  # Check if the parent directory of /var/vroom/sentry-profiles is already owned by vroom:vroom
+  if [ "$($dcr --no-deps --entrypoint /bin/bash --user root vroom -c "stat -c '%U:%G' /var/vroom/sentry-profiles" 2>/dev/null)" = "vroom:vroom" ]; then
+    echo "Ownership of /var/vroom/sentry-profiles is already set to vroom:vroom. Skipping chown."
+  else
+    $dcr --no-deps --entrypoint /bin/bash --user root vroom -c 'chown -R vroom:vroom /var/vroom/sentry-profiles && chmod -R o+rwx /var/vroom/sentry-profiles'
+  fi
+
+  echo "${_endgroup}"
 fi
-
-echo "${_group}Ensuring correct permissions on profiles directory ..."
-
-# Check if the parent directory of /var/vroom/sentry-profiles is already owned by vroom:vroom
-if [ "$($dcr --no-deps --entrypoint /bin/bash --user root vroom -c "stat -c '%U:%G' /var/vroom/sentry-profiles" 2>/dev/null)" = "vroom:vroom" ]; then
-  echo "Ownership of /var/vroom/sentry-profiles is already set to vroom:vroom. Skipping chown."
-else
-  $dcr --no-deps --entrypoint /bin/bash --user root vroom -c 'chown -R vroom:vroom /var/vroom/sentry-profiles && chmod -R o+rwx /var/vroom/sentry-profiles'
-fi
-
-echo "${_endgroup}"
