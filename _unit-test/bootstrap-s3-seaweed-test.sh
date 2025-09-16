fd07cfc63f2d02356b43012414e7c7ff5049f2d8
Reinaldy Rafli <github@reinaldyrafli.com>
2025-09-16 21:34:17 +0700

    fix: able to setup nodestore multiple times

---
+++ _unit-test/bootstrap-s3-seaweed-test.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+source _unit-test/_test_setup.sh
+source install/dc-detect-version.sh
+
+# Set the flag to apply automatic updates
+export APPLY_AUTOMATIC_CONFIG_UPDATES=1
+
+# Here we're just gonna test to run it multiple times
+# Only to make sure it doesn't break
+for i in $(seq 1 5); do
+  source install/bootstrap-s3-nodestore.sh
+done
+
+report_success
