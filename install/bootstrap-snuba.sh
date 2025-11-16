1c82856d2473576577bf2810c9046f93cf8286a0
Reinaldy Rafli <github@reinaldyrafli.com>
2025-11-16 19:31:30 +0700

    fix: use host's ulimit

--- install/bootstrap-snuba.sh
+++ install/bootstrap-snuba.sh
@@ -1,11 +1,6 @@
 echo "${_group}Bootstrapping and migrating Snuba ..."
 
 if [[ -z "${SKIP_SNUBA_MIGRATIONS:-}" ]]; then
-  # NOTE(aldy505): Temporarily increase the number of open files to avoid errors
-  # Otherwise, we'll get "crun: setrlimit `RLIMIT_NOFILE`: Operation not permitted: OCI permission denied"
-  if [[ "$CONTAINER_ENGINE" == "podman" ]]; then
-    ulimit -u 100000
-  fi
   $dcr snuba-api bootstrap --force
 else
   echo "Skipped DB migrations due to SKIP_SNUBA_MIGRATIONS=$SKIP_SNUBA_MIGRATIONS"
