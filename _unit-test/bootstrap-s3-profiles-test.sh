f0d3d29a518a70564a3aaba79a1cc2e87c87f51a
Reinaldy Rafli <github@reinaldyrafli.com>
2025-11-27 14:47:28 +0700

    ref: volume migration tests

---
+++ _unit-test/bootstrap-s3-profiles-test.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+source _unit-test/_test_setup.sh
+source install/dc-detect-version.sh
+source install/create-docker-volumes.sh
+
+# Generate some random files on `sentry-vroom` volume for testing
+$dc run --rm --no-deps -v sentry-vroom:/var/vroom/sentry-profiles busybox sh -c '
+  for i in $(seq 1 1000); do
+    echo "This is test file $i" > /var/vroom/sentry-profiles/test_file_$i.txt
+  done
+'
+
+# Set the flag to apply automatic updates
+export APPLY_AUTOMATIC_CONFIG_UPDATES=1
+
+# Here we're just gonna test to run it multiple times
+# Only to make sure it doesn't break
+for i in $(seq 1 5); do
+  source install/bootstrap-s3-vroom.sh
+done
+
+# Ensure that the files have been migrated to SeaweedFS
+migrated_files_count=$($dc run --rm --no-deps seaweedfs sh -c '
+  apk add --no-cache s3cmd &&
+  s3cmd --access_key=sentry --secret_key=sentry --no-ssl --region=us-east-1 --host=localhost:8333 --host-bucket="localhost:8333/%(bucket)" ls s3://profiles/ | wc -l
+')
+if [[ "$migrated_files_count" -ne 1000 ]]; then
+  echo "Error: Expected 1000 migrated files, but found $migrated_files_count"
+  exit 1
+fi
+
+report_success
