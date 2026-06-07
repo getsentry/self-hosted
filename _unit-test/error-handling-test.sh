7ccd4441e2d0e2383742a08bbef6f686a86e8c86
Reinaldy Rafli <github@reinaldyrafli.com>
2026-06-07 20:21:19 +0700

    debug: changed source line

--- _unit-test/error-handling-test.sh
+++ _unit-test/error-handling-test.sh
@@ -63,6 +63,7 @@ export dc=':'
 echo "Test Logs" >"$log_file"
 CLEANUP_RESPONSE=$(cleanup ERROR) # the linenumber of this line must match just below
 rm "$log_file"
+echo $CLEANUP_RESPONSE
 test "$CLEANUP_RESPONSE" == 'Error in _unit-test/error-handling-test.sh:63.
 '\''local cmd="${BASH_COMMAND}"'\'' exited with status 0
 
@@ -76,6 +77,7 @@ export REPORT_SELF_HOSTED_ISSUES=0
 export MINIMIZE_DOWNTIME=1
 echo "Test Logs" >"$log_file"
 CLEANUP_RESPONSE=$(cleanup ERROR) # the linenumber of this line must match just below
+echo $CLEANUP_RESPONSE
 rm "$log_file"
 test "$CLEANUP_RESPONSE" == 'Error in _unit-test/error-handling-test.sh:77.
 '\''local cmd="${BASH_COMMAND}"'\'' exited with status 0
