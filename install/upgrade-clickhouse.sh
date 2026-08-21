ca99ce5edf70935267cf302ae54e505e9a7186c0
Reinaldy Rafli <github@reinaldyrafli.com>
2026-08-21 08:48:27 +0700

    chore: cleanup old logic
    We have hard stop on 26.7.0, people are supposed to run 25.3 already

--- install/upgrade-clickhouse.sh
+++ install/upgrade-clickhouse.sh
@@ -18,52 +18,12 @@ if $ps_command | grep -q clickhouse; then
   version=$($dc exec clickhouse clickhouse-client -q 'SELECT version()')
   if [[ "$version" == "25.3.6.10034.altinitystable" ]]; then
     echo "Detected clickhouse version $version"
-    start_service_and_wait_ready clickhouse
     $dc down clickhouse
 
     echo "Upgrading clickhouse to 25.8"
     $dcb $build_arg BASE_IMAGE=altinity/clickhouse-server:25.8.28.10001.altinitystable clickhouse
     start_service_and_wait_ready clickhouse
     $dc down clickhouse
-  elif [[ "$version" == "21.8.13.1.altinitystable" || "$version" == "21.8.12.29.altinitydev.arm" ]]; then
-    echo "Detected clickhouse version $version"
-    $dc down clickhouse
-
-    echo "Upgrading clickhouse to 22.8"
-    $dcb $build_arg BASE_IMAGE=altinity/clickhouse-server:22.8.15.25.altinitystable clickhouse
-    start_service_and_wait_ready clickhouse
-    $dc down clickhouse
-
-    echo "Upgrading clickhouse to 23.3"
-    $dcb $build_arg BASE_IMAGE=altinity/clickhouse-server:23.3.19.33.altinitystable clickhouse
-    start_service_and_wait_ready clickhouse
-    $dc down clickhouse
-
-    echo "Upgrading clickhouse to 23.8"
-    $dcb $build_arg BASE_IMAGE=altinity/clickhouse-server:23.8.11.29.altinitystable clickhouse
-    start_service_and_wait_ready clickhouse
-    $dc down clickhouse
-
-    echo "Upgrading clickhouse to 24.8"
-    $dcb $build_arg BASE_IMAGE=altinity/clickhouse-server:24.8.14.10459.altinitystable clickhouse
-    start_service_and_wait_ready clickhouse
-    $dc down clickhouse
-
-    echo "Upgrading clickhouse to 25.3"
-    $dcb $build_arg BASE_IMAGE=altinity/clickhouse-server:25.3.6.10034.altinitystable clickhouse
-    start_service_and_wait_ready clickhouse
-  elif [[ "$version" == "23.8.11.29.altinitystable" ]]; then
-    echo "Detected clickhouse version $version"
-    $dc down clickhouse
-
-    echo "Upgrading clickhouse to 24.8"
-    $dcb $build_arg BASE_IMAGE=altinity/clickhouse-server:24.8.14.10459.altinitystable clickhouse
-    start_service_and_wait_ready clickhouse
-    $dc down clickhouse
-
-    echo "Upgrading clickhouse to 25.3"
-    $dcb $build_arg BASE_IMAGE=altinity/clickhouse-server:25.3.6.10034.altinitystable clickhouse
-    start_service_and_wait_ready clickhouse
   else
     echo "Detected clickhouse version $version. Skipping upgrades!"
   fi
